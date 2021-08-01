4b58f074f0ebae72de1a7bdea93ace20ff35b48a
Yayat Supriyatna <yayat.supriyatna@majapahit.id>
2021-08-01 13:02:20 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ module.exports = {
     domains: ['api.dts.majapahit.id'],
   },
   env: {
-    END_POINT_API: 'http://dts-publikasi:80',
-    END_POINT_API_IMAGE: 'http://dts-publikasi:80/storage/',
+    END_POINT_API: 'http://dts-publikasi-dev.majapahit.id/',
+    END_POINT_API_IMAGE: 'http://dts-publikasi-dev.majapahit.id//storage/',
     END_POINT_TOKEN_API: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImlzcyI6InNuSzEyd3hLQ1hRSzRYdHU2azBLV29QaVN6U1hTUmxNIn0.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Im5hbWEgZHV1ZHUiLCJleHAiOjE2MjcxMjk5Mzd9.f8dRFus2HnsJ22_KDnwvlvJ_k3efz_8bdJ2JCbET8uA',
     END_POINT_KEY_AUTH: '12131231231210923123',
   },
