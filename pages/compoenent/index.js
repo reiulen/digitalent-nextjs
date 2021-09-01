d1183647def9a8c87a5e38ddb79698c32fd3f369
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2021-09-01 16:56:38 +0700

    prepare component

--- pages/compoenent/index.js
+++ pages/compoenent/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function index() {
   return (
     <div className="p-4">
-      <h1>Compoenent</h1>
+      <h1>Compoenent OTW</h1>
       {/* button rounded full + icon */}
       <button className="btn btn-secondary">test</button>
     </div>
