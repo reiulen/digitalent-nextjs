50fd7dd29c6b6b19e76718ddc0c5e4138f7118dc
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2021-09-02 10:47:43 +0700

    merge

--- pages/compoenent/index.js
+++ pages/compoenent/index.js
@@ -56,6 +56,7 @@ export default function index() {
       </Link>
       {/* btn action */}
       {/* eye */}
+
       <button className="btn btn-link-action bg-red-primary">
         <svg
           xmlns="http://www.w3.org/2000/svg"
