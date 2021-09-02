59767df282f54f653dabe0a068a28c1138616dd3
dendyjuliano <dendyjuliano2019@gmail.com>
2021-09-02 11:09:13 +0700

    dynamic

--- pages/subvit/substansi/index.js
+++ pages/subvit/substansi/index.js
@@ -2,29 +2,31 @@ import React, { useEffect, useState } from "react";
 
 import dynamic from "next/dynamic";
 import Layout from "../../../components/templates/layout.component";
-// import ListSubstansi from '../../../components/content/subvit/substansi/list-substansi'
-const ListSubstansi = dynamic(() =>
-  import("../../../components/content/subvit/substansi/list-substansi")
-);
 import LoadingPage from "../../../components/LoadingPage";
+// import ListSubstansi from '../../../components/content/subvit/substansi/list-substansi'
+const ListSubstansi = dynamic(
+  () => import("../../../components/content/subvit/substansi/list-substansi"),
+  { loading: () => <LoadingPage /> }
+);
 
 import { getAllSubtanceQuestionBanks } from "../../../redux/actions/subvit/subtance.actions";
 import { wrapper } from "../../../redux/store";
 
 export default function Substansi() {
-  const [loader, setLoader] = useState(true);
+  //   const [loader, setLoader] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoader(false);
-    }, 2000);
-  }, [setLoader]);
+  //   useEffect(() => {
+  //     setTimeout(() => {
+  //       setLoader(false);
+  //     }, 2000);
+  //   }, [setLoader]);
 
   return (
     <>
       <div className="d-flex flex-column flex-root">
         <Layout title="List Test Substansi">
-          {loader ? <LoadingPage loading={true} /> : <ListSubstansi />}
+          {/* {loader ? <LoadingPage loading={true} /> : <ListSubstansi />} */}
+          <ListSubstansi />
         </Layout>
       </div>
     </>
