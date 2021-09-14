682d8eea031b437c4b529c200a96c3384400ba9d
fandiario <fandi.ario10@gmail.com>
2021-09-14 12:03:00 +0700

    feature/s2_49_dashboard-publikasi

--- components/content/publikasi/dashboard-publikasi/dashboard-publikasi.jsx
+++ components/content/publikasi/dashboard-publikasi/dashboard-publikasi.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import Image from 'next/image'
-import { BarChart, Bar, PieChart, Pie, Cell } from 'recharts';
+import { BarChart, Bar, PieChart, Pie, Cell, Tooltip, Legend, XAxis } from 'recharts';
 
 import CardDashboard from '../../../CardDashboard'
 import PageWrapper from '../../../wrapper/page.wrapper'
@@ -8,7 +8,7 @@ import PageWrapper from '../../../wrapper/page.wrapper'
 const DashbardPublikasi = () => {
 
     // const [ openBook, setOpenBook ] = useState ("/assets/new/open-book.svg")
-    const colors = ["#215480", "#4299E1"]
+    const colors = [ "#4299E1", "#215480"]
     const [ dataBarChart, setDataBarChart ] = useState ([
         {
             "name": "Page A",
@@ -62,12 +62,12 @@ const DashbardPublikasi = () => {
     const [ dataPieChart, setDataPieChart ] = useState ([
         {
             "name": "Peserta",
-            "value": 400
+            "value": 100
         },
 
         {
             "name": "Author",
-            "value": 100
+            "value": 400
         },
     ])
 
@@ -115,6 +115,7 @@ const DashbardPublikasi = () => {
                                 <h3 className="card-title font-weight-bolder text-light mt-5">Total Publish dan Unpublish</h3>
                                 <div className="d-flex align-items-center justify-content-center">
                                     <BarChart width={450} height={350} data={dataBarChart}>
+                                        <XAxis dataKey="name" hide={true}/>
                                         <Bar 
                                             dataKey="publish" 
                                             fill="#4299E1" 
@@ -127,6 +128,10 @@ const DashbardPublikasi = () => {
                                             barSize={10} 
                                             radius={[10, 10, 0, 0]}
                                         />
+                                        <Tooltip 
+                                            cursor={{fill:"transparent"}}
+                                        />
+                                        
                                     </BarChart>
                                 </div>
                             </div>
@@ -178,6 +183,7 @@ const DashbardPublikasi = () => {
                                 <div className="text-muted">
                                     Total User
                                 </div>
+                                
                                 <div className="d-flex align-items-center justify-content-center">
                                     <PieChart width={450} height={350}>
                                         <Pie 
@@ -186,8 +192,10 @@ const DashbardPublikasi = () => {
                                             // nameKey="name"
                                             cx="50%" 
                                             cy="50%" 
-                                            innerRadius={60} 
-                                            outerRadius={80} 
+                                            innerRadius={115} 
+                                            outerRadius={140} 
+                                            paddingAngle={-10}
+                                            cornerRadius={30}
                                             // fill="#215480"
                                         >
                                             {
@@ -198,12 +206,23 @@ const DashbardPublikasi = () => {
                                                 })
                                             } 
                                         </Pie>
+                                        <Tooltip 
+                                            cursor={{fill:"transparent"}}
+                                        />
                                         
                                     </PieChart>
+                                    
                                 </div>
+
+                                <div className="d-flex align-items-center justify-content-center" style={{marginTop:"-28vh"}}>
+                                    <h1 className="font-weight-bolder display-2">
+                                        133
+                                    </h1>
+                                </div> 
+                                
                             </div>
-                            <div className="card-body">
-                                <div className="mb-10 flex-column">
+                            <div className="card-body" style={{marginTop:"20vh"}}>
+                                <div className="mb-10 flex-column ">
                                     {/* <h3 className="card-title font-weight-bolder text-muted">Total Konten</h3> */}
                                     <div className="row">
                                         <div className="col-6 d-flex flex-row d-flex justify-content-center">
