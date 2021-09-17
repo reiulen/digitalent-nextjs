d6ad4beae048d0669c00e6c41934f60aecee988c
fandiario <fandi.ario10@gmail.com>
2021-09-17 10:01:28 +0700

    init

--- components/content/publikasi/artikel-peserta/edit.jsx
+++ components/content/publikasi/artikel-peserta/edit.jsx
@@ -88,6 +88,8 @@ const EditArtikel = () => {
   const [users_id, setUserId] = useState(artikel_peserta.users_id);
   const [tag, setTag] = useState(artikel_peserta.tag);
   const [publish, setPublish] = useState(artikel_peserta.publish === 1 ? true : false);
+  const [publishDate, setPublishDate] = useState(artikel_peserta.tanggal_publish ? new Date (artikel.tanggal_publish) : null);
+  const [disablePublishDate, setDisablePublishDate] = useState(artikel_peserta.publish === 0 ? true : false)
   const [_method, setMethod] = useState("put");
 
   const onChangeGambar = (e) => {
@@ -124,9 +126,26 @@ const EditArtikel = () => {
   };
 
   const handleChangePublish = (e) => {
-    setPublish(e.target.checked);
+    // setPublish(e.target.checked);
+    setDisablePublishDate(!disablePublishDate)
     // console.log (e.target.checked)
+
+    if (e.target.checked === false){
+        setPublishDate (null)
+        setPublish (0)
+    } else {
+        setPublish (1)
+    }
   };
+
+  const handlePublishDate = (date) => {
+    // let result = moment(date).format("YYYY-MM-DD")
+    if (disablePublishDate === false) {
+      // setPublishDate(result)
+      setPublishDate(date)
+      // console.log (result)
+    }
+  }
 
   const onSubmit = (e) => {
     console.log ("test-0")
@@ -166,6 +185,7 @@ const EditArtikel = () => {
           publish,
           id,
           _method,
+          tanggal_publish : moment(publishDate).format("YYYY-MM-DD")
         };
         
         Swal.fire({
@@ -201,6 +221,7 @@ const EditArtikel = () => {
           publish,
           id,
           _method,
+          tanggal_publish : moment(publishDate).format("YYYY-MM-DD")
         };
         
         Swal.fire({
@@ -319,7 +340,7 @@ const EditArtikel = () => {
         ) : (
           ""
         )}
-        {success ? (
+        {/* {success ? (
           <div
             className="alert alert-custom alert-light-success fade show mb-5"
             role="alert"
@@ -344,12 +365,12 @@ const EditArtikel = () => {
           </div>
         ) : (
           ""
-        )}
+        )} */}
 
         <div className="col-lg-12 col-xxl-12 order-1 order-xxl-2 px-0">
           {loading ? <LoadingPage loading={loading} /> : ""}
           <div className="card card-custom card-stretch gutter-b">
-            <div className="card-header border-0">
+            <div className="card-header">
               <h3 className="card-title font-weight-bolder text-dark">
                 Ubah Artikel Peserta
               </h3>
@@ -496,6 +517,13 @@ const EditArtikel = () => {
                         null
                     }
                   </div>
+
+                  <div className="mt-3 col-sm-3 text-muted">
+                    <p>
+                      Resolusi yang direkomendasikan adalah 1024 * 512. Fokus visual pada bagian tengah gambar
+                    </p>
+                      
+                  </div>
                   
                 </div>
 
@@ -548,7 +576,7 @@ const EditArtikel = () => {
                   <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Kategori</label>
                   <div className="col-sm-12">
                       <select name="" id="" className='form-control' value={kategori_id} onChange={e => setKategoriId(e.target.value)} onBlur={e => { setKategoriId(e.target.value); simpleValidator.current.showMessageFor('kategori_id') }} >
-                          <option selected disabled value=''>-- Kategori --</option>
+                          <option selected disabled value=''>-- Artikel --</option>
                           {!kategori || (kategori && kategori.length === 0) ? (
                               <option value="">Data kosong</option>
                           ) : (
@@ -631,11 +659,40 @@ const EditArtikel = () => {
                   </div> */}
                 </div>
 
+                <div className="form-group">
+                  <label className='col-sm-5 col-form-label font-weight-bolder'>Set Tanggal Publish</label>
+                  <div className="col-sm-12">
+                    <div className="input-group">
+                      <DatePicker
+                        className="form-search-date form-control-sm form-control"
+                        selected={publishDate}
+                        onChange={(date) => handlePublishDate(date)}
+                        // onChange={(date) => setPublishDate(date)}
+                        selectsStart
+                        startDate={publishDate}
+                        // endDate={endDate}
+                        dateFormat="dd/MM/yyyy"
+                        placeholderText="Silahkan Isi Tanggal Publish"
+                        wrapperClassName="col-12 col-lg-12 col-xl-12"
+                        minDate={moment().toDate()}
+                      // minDate={addDays(new Date(), 20)}
+                        disabled = {disablePublishDate === true || disablePublishDate === null}
+                      />
+                    </div>
+                    {
+                      disablePublishDate === true ?
+                        <small className="text-muted">Harap ubah status publikasi menjadi aktif untuk mengisi Tanggal Publish</small>
+                      :
+                        null
+                    }
+                  </div>
+                </div>
+
                 <div className="form-group row">
                   <div className="col-sm-2"></div>
                   <div className="col-sm-10 text-right">
                     <Link href="/publikasi/artikel-peserta">
-                      <a className="btn btn-outline-primary-rounded-full rounded-pill mr-2 btn-sm">
+                      <a className="btn btn-white-ghost-rounded-full rounded-pill mr-2 btn-sm">
                         Kembali
                       </a>
                     </Link>
