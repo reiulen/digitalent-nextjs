11df68a533bf088990c97f7b6e1dfede85ea9acb
dendyjuliano <dendyjuliano2019@gmail.com>
2021-09-21 06:24:37 +0700

    nextauth config

--- pages/subvit/trivia/index.js
+++ pages/subvit/trivia/index.js
@@ -1,52 +1,52 @@
-import dynamic from 'next/dynamic';
-import LoadingSkeleton from '../../../components/LoadingSkeleton';
+import dynamic from "next/dynamic";
+import LoadingSkeleton from "../../../components/LoadingSkeleton";
 
 // import ListTrivia from "/components/content/subvit/trivia/list-trivia";
 const ListTrivia = dynamic(
-    () => import('../../../components/content/subvit/trivia/list-trivia'),
-    { suspense: true, loading: () => <LoadingSkeleton /> }
+  () => import("../../../components/content/subvit/trivia/list-trivia"),
+  { suspense: true, loading: () => <LoadingSkeleton /> }
 );
 
-import { getAllTriviaQuestionBanks } from '../../../redux/actions/subvit/trivia-question.actions';
-import { wrapper } from '../../../redux/store';
-import { getSession } from 'next-auth/client';
+import { getAllTriviaQuestionBanks } from "../../../redux/actions/subvit/trivia-question.actions";
+import { wrapper } from "../../../redux/store";
+import { getSession } from "next-auth/client";
 
 export default function Trivia(props) {
-    const session = props.session.user.user.data;
+  const session = props.session.user.user.data;
 
-    return (
-        <>
-            <div className="d-flex flex-column flex-root">
-                <ListTrivia token={session.token} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="d-flex flex-column flex-root">
+        <ListTrivia token={session.token} />
+      </div>
+    </>
+  );
 }
 
 export const getServerSideProps = wrapper.getServerSideProps(
-    store =>
-        async ({ query, req }) => {
-            const session = await getSession({ req });
-            if (!session) {
-                return {
-                    redirect: {
-                        destination: '/',
-                        permanent: false,
-                    },
-                };
-            }
+  (store) =>
+    async ({ query, req }) => {
+      const session = await getSession({ req });
+      if (!session) {
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
 
-            await store.dispatch(
-                getAllTriviaQuestionBanks(
-                    query.page,
-                    query.keyword,
-                    query.limit,
-                    session.user.user.data.token
-                )
-            );
+      await store.dispatch(
+        getAllTriviaQuestionBanks(
+          query.page,
+          query.keyword,
+          query.limit,
+          session.user.user.data.token
+        )
+      );
 
-            return {
-                props: { session, title: 'Trivia - Subvit' },
-            };
-        }
+      return {
+        props: { session, title: "Trivia - Subvit" },
+      };
+    }
 );
