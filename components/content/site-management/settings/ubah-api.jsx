d7fc7b1fa21855652b43f8dc10a625298573443b
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2021-09-28 16:40:43 +0700

    slicing setting site management

---
+++ components/content/site-management/settings/ubah-api.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Pagination from "react-js-pagination";
+import PageWrapper from "../../../wrapper/page.wrapper";
+import { useDispatch, useSelector } from "react-redux";
+import LoadingTable from "../../../LoadingTable";
+import IconEye from "../../../assets/icon/Eye";
+import IconPencil from "../../../assets/icon/Pencil";
+import IconDelete from "../../../assets/icon/Delete";
+import IconAdd from "../../../assets/icon/Add";
+import IconSearch from "../../../assets/icon/Search";
+
+const UbahApi = ({ token }) => {
+  let dispatch = useDispatch();
+  const router = useRouter();
+
+  const onNewReset = () => {
+    router.replace("/site-management/api", undefined, {
+      shallow: true,
+    });
+  };
+  return (
+    <PageWrapper>
+      <div className="col-lg-12 order-1 px-0">
+        <div className="card card-custom card-stretch gutter-b">
+          <div className="card-header border-0">
+            <h3
+              className="card-title font-weight-bolder text-dark border-bottom w-100 pb-5 mb-5 mt-5"
+              style={{ fontSize: "24px" }}
+            >
+              Ubah API
+            </h3>
+          </div>
+          <div className="card-body pt-0">
+            <form>
+              <div class="form-group">
+                <label>Nama API</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Placeholder"
+                />
+                <span class="form-text text-muted">
+                    Please enter your full name
+                  </span>
+              </div>
+              <div class="form-group">
+                <label>Nama Pengguna</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Placeholder"
+                />
+                <span class="form-text text-muted">
+                    Please enter your full name
+                  </span>
+              </div>
+              <div class="form-group">
+                <label for="exampleSelect1">Status</label>
+                <select class="form-control" id="exampleSelect1">
+                  <option>Placeholder</option>
+                </select>
+                <span class="form-text text-muted">
+                    Please enter your full name
+                  </span>
+              </div>
+              <div class="form-group">
+                <label for="exampleSelect1">Pilih API</label>
+                <select class="form-control" id="exampleSelect1">
+                  <option>Placeholder</option>
+                </select>
+                <span class="form-text text-muted">
+                    Please enter your full name
+                  </span>
+              </div>
+              <div class="form-group">
+                <label for="exampleSelect1">Field</label>
+                <select class="form-control" id="exampleSelect1">
+                  <option>Placeholder</option>
+                </select>
+                <span class="form-text text-muted">
+                    Please enter your full name
+                  </span>
+              </div>
+              <div class="form-group row">
+                <div class="col-lg-6">
+                  <label>From</label>
+                  <input
+                    type="date"
+                    class="form-control"
+                    placeholder="Enter full name"
+                  />
+                  <span class="form-text text-muted">
+                    Please enter your full name
+                  </span>
+                </div>
+                <div class="col-lg-6">
+                  <label>To</label>
+                  <input
+                    type="date"
+                    class="form-control"
+                    placeholder="Enter contact number"
+                  />
+                  <span class="form-text text-muted">
+                    Please enter your contact number
+                  </span>
+                </div>
+              </div>
+            </form>
+            <div className="form-group row">
+                <div className="col-sm-12 d-flex justify-content-end">
+                  <Link href="/partnership/master-kategori-kerjasama">
+                    <a className="btn btn-sm btn-white btn-rounded-full text-blue-primary mr-5">
+                      Kembali
+                    </a>
+                  </Link>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-rounded-full bg-blue-primary text-white"
+                  >
+                    Simpan
+                  </button>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default UbahApi;
---
+++ pages/site-management/setting/api/tambah-api/index.js
@@ -0,0 +1,59 @@
+import dynamic from "next/dynamic";
+import { getSession } from "next-auth/client";
+// import { getAllArtikel } from "../../../redux/actions/publikasi/artikel.actions";
+import { wrapper } from "../../../../../redux/store";
+import LoadingPage from "../../../../../components/LoadingPage";
+
+const TambahApi = dynamic(
+  () =>
+    import(
+      "../../../../../components/content/site-management/settings/tambah-api"
+    ),
+  {
+    loading: function loadingNow() {
+      return <LoadingPage />;
+    },
+    ssr: false,
+  }
+);
+
+export default function TambahPage(props) {
+  const session = props.session.user.user.data;
+  return (
+    <>
+      <div className="d-flex flex-column flex-root">
+        <TambahApi token={session.token} />
+      </div>
+    </>
+  );
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ query, req }) => {
+      const session = await getSession({ req });
+      if (!session) {
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+
+      // await store.dispatch(
+      //   getAllArtikel(
+      //     query.page,
+      //     query.keyword,
+      //     query.limit,
+      //     query.publish,
+      //     query.startdate,
+      //     query.enddate,
+      //     session.user.user.data.token
+      //   )
+      // );
+      return {
+        props: { session, title: "Tambah API - Site Management" },
+      };
+    }
+);
