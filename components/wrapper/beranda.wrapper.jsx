5c66f9cd63a24747763001f011922f6cf3753fa5
fandiario <fandi.ario10@gmail.com>
2021-10-07 16:43:56 +0700

    feature/s1_96_homepage-fe

---
+++ components/wrapper/beranda.wrapper.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from "next/head";
+import "../../styles/beranda.module.css"
+
+const BerandaWrapper = ({ children, title }) => {
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <div className="d-flex flex-column-fluid "style={{backgroundColor: "#ffffff"}}>
+                <div className="container">
+                    {children}
+                </div>
+            </div>
+        </>
+        
+    )
+}
+
+export default BerandaWrapper
