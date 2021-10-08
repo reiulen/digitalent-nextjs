9b462f53cf2eda5e6705cc89241fec47bbb8bc0b
ahmadfiraz.mjph <ahmadfiraz@majapahit.id>
2021-10-08 07:31:24 +0700

    adding edit pages function

--- components/content/sertifikat/kelola-sertifikat/nama_pelatihan/nama_pelatihan.jsx
+++ components/content/sertifikat/kelola-sertifikat/nama_pelatihan/nama_pelatihan.jsx
@@ -54,10 +54,10 @@ export default function NamaPelatihanID({ token }) {
     router.push(link);
   };
 
-  console.log("masuk halaman ini");
-  useEffect(() => {
-    console.log(loading, "INI LOADING");
-  }, [loading]);
+  // console.log("masuk halaman ini");
+  // useEffect(() => {
+  //   console.log(loading, "INI LOADING");
+  // }, [loading]);
 
   return (
     <PageWrapper>
