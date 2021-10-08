992bf6b8b999e11c61bb1353c708cc95dfc04025
dendyjuliano <dendyjuliano2019@gmail.com>
2021-10-08 14:21:49 +0700

    conflick

--- user-component/content/peserta/profile/index.jsx
+++ user-component/content/peserta/profile/index.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { Row, Col, Card, Figure, Button, ButtonGroup } from "react-bootstrap";
+import Image from "next/image";
+import Link from "next/link";
+import style from "../../../../styles/peserta/dashboard.module.css";
 
 const Profile = () => {
   return (
     <>
-      <h1>Profile</h1>
+      <div className="container-fluid p-6">
+        <Row>
+          <Col md={3}>
+            <Card className="card-custom card-stretch gutter-b">
+              <Card.Body className="mx-auto">
+                <Figure>
+                  <Image
+                    width={237}
+                    height={256}
+                    src="/assets/media/mitra-icon/laravel-1.svg"
+                    objectFit="cover"
+                    className={style.figure_img}
+                  />
+                </Figure>
+                <Button
+                  className="btn-outline-primary rounded-pill btn-block font-weight-bold"
+                  size="lg"
+                >
+                  Ubah Data
+                </Button>
+
+                <ButtonGroup vertical>
+                  <Button className="mb-2">Button</Button>
+                  <Button className="mb-2">Button</Button>
+                </ButtonGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={9} className="order-1">
+            <Card className="card-custom card-stretch gutter-b">
+              <Card.Body></Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </>
   );
 };
