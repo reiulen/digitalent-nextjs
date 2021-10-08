d4493e60deebad404e1627529fe0a4dd72cc1725
dendyjuliano <dendyjuliano2019@gmail.com>
2021-10-09 03:18:27 +0700

    edit profile peserta pelatihan

---
+++ user-component/content/peserta/profile/edit/pekerjaan.edit.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PekerjaanEdit = () => {
+  return (
+    <>
+      <h1>Edit Pekerjaan</h1>
+    </>
+  );
+};
+
+export default PekerjaanEdit;
--- user-component/content/peserta/profile/index.jsx
+++ user-component/content/peserta/profile/index.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Card, Figure, Button, ButtonGroup } from "react-bootstrap";
 import Image from "next/image";
 import Link from "next/link";
-import style from "../../../../styles/peserta/dashboard.module.css";
+import style from "../../../../styles/peserta/profile.module.css";
+
+//information
+import Informasi from "./informasi";
+import Alamat from "./alamat";
+import Pendidikan from "./pendidikan";
+import Pekerjaan from "./pekerjaan";
+
+//edit information
+import InformasiEdit from "./edit/informasi.edit";
+import AlamatEdit from "./edit/alamat.edit";
+import PendidikanEdit from "./edit/pendidikan.edit";
+import PekerjaanEdit from "./edit/pekerjaan.edit";
 
 const Profile = () => {
+  const [viewProfile, setViewProfile] = useState(1);
+  const [viewEdit, setViewEdit] = useState(false);
+
+  const handleViewProfile = () => {
+    switch (viewProfile) {
+      case 1:
+        return viewEdit ? <InformasiEdit /> : <Informasi />;
+        break;
+      case 2:
+        return viewEdit ? <AlamatEdit /> : <Alamat />;
+        break;
+      case 3:
+        return viewEdit ? <PendidikanEdit /> : <Pendidikan />;
+        break;
+      case 4:
+        return viewEdit ? <PekerjaanEdit /> : <Pekerjaan />;
+        break;
+      default:
+        return <Informasi />;
+        break;
+    }
+  };
   return (
     <>
       <div className="container-fluid p-6">
@@ -22,22 +56,89 @@ const Profile = () => {
                   />
                 </Figure>
                 <Button
-                  className="btn-outline-primary rounded-pill btn-block font-weight-bold"
+                  className={`${
+                    viewEdit
+                      ? "btn-primary-rounded-full bg-blue-primary"
+                      : "btn-outline-primary"
+                  } rounded-pill btn-block font-weight-bold`}
                   size="lg"
+                  onClick={() => setViewEdit(!viewEdit)}
                 >
                   Ubah Data
                 </Button>
 
-                <ButtonGroup vertical>
-                  <Button className="mb-2">Button</Button>
-                  <Button className="mb-2">Button</Button>
+                <ButtonGroup vertical className="mt-4 btn-block ">
+                  <Button
+                    className={`mb-2 font-weight-bold text-left py-4 ${
+                      viewProfile === 1
+                        ? style.btn_profile_active
+                        : style.btn_profile
+                    }`}
+                    variant="transparent"
+                    onClick={() => setViewProfile(1)}
+                  >
+                    <i
+                      className={`ri-user-3-fill mr-3 ${
+                        viewProfile === 1 ? style.icon_active : ""
+                      }`}
+                    ></i>
+                    Informasi Pribadi
+                  </Button>
+                  <Button
+                    className={`mb-2 font-weight-bold text-left py-4 ${
+                      viewProfile === 2
+                        ? style.btn_profile_active
+                        : style.btn_profile
+                    }`}
+                    variant="transparent"
+                    onClick={() => setViewProfile(2)}
+                  >
+                    <i
+                      className={`ri-map-pin-line mr-3 ${
+                        viewProfile === 2 ? style.icon_active : ""
+                      }`}
+                    ></i>
+                    Alamat
+                  </Button>
+                  <Button
+                    className={`mb-2 font-weight-bold text-left py-4 ${
+                      viewProfile === 3
+                        ? style.btn_profile_active
+                        : style.btn_profile
+                    }`}
+                    variant="transparent"
+                    onClick={() => setViewProfile(3)}
+                  >
+                    <i
+                      className={`ri-book-open-fill mr-3 ${
+                        viewProfile === 3 ? style.icon_active : ""
+                      }`}
+                    ></i>
+                    Pendidikan
+                  </Button>
+                  <Button
+                    className={`mb-2 font-weight-bold text-left py-4 ${
+                      viewProfile === 4
+                        ? style.btn_profile_active
+                        : style.btn_profile
+                    }`}
+                    variant="transparent"
+                    onClick={() => setViewProfile(4)}
+                  >
+                    <i
+                      className={`ri-handbag-fill mr-3 ${
+                        viewProfile === 4 ? style.icon_active : ""
+                      }`}
+                    ></i>
+                    Pekerjaan
+                  </Button>
                 </ButtonGroup>
               </Card.Body>
             </Card>
           </Col>
           <Col md={9} className="order-1">
-            <Card className="card-custom card-stretch gutter-b">
-              <Card.Body></Card.Body>
+            <Card className="card-custom gutter-b">
+              <Card.Body>{handleViewProfile()}</Card.Body>
             </Card>
           </Col>
         </Row>
