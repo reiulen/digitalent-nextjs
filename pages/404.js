536523e62e39dabdab976f6d78251f50757a2614
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2021-10-15 03:43:50 +0700

    page not found fix

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,19 @@
 import { useEffect } from "react";
 import { Router, useRouter } from "next/router";
 export default function Custom404() {
-
   const router = useRouter();
-  
+
   useEffect(() => {
     setTimeout(() => {
       router.push("/");
-    }, 1000);
+    }, 3000);
   });
 
   return (
-    <div>
-      <h1>Oooooo....</h1>
-      <h1>Halaman yang anda cari tidak ditemukan</h1>
+    <div className="not-found-page">
+      <h1 className="center-absolute" style={{ fontSize: "" }}>
+        Halaman yang anda cari tidak ditemukan !! ....
+      </h1>
     </div>
   );
 }
