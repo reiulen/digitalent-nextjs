3e748a701eecce8568f792ac123a27e9d3f0bea4
Alfin <alfinramadhan@majapahit.id>
2021-10-21 16:03:09 +0700

    remaining minor bug and responsive done major

--- components/content/subvit/dashboard/dashboard-subvit.jsx
+++ components/content/subvit/dashboard/dashboard-subvit.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -17,18 +17,182 @@ import {
   ResponsiveContainer,
   Label,
   Legend,
+  Tooltip,
 } from "recharts";
 import { useRouter } from "next/dist/client/router";
-import { Tooltip } from "bootstrap";
 
 // import { getSession } from "next-auth/client";
 
 const DashbardSubvit = () => {
-  const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 200 },
-    { name: "Group C", value: 500 },
-  ];
+  const [dataDummy, setDataDummy] = useState({
+    status: true,
+    message: "Berhasil Mendapatkan Data",
+    data: {
+      substansi: {
+        perPage: 5,
+        total: 13,
+        totalFiltered: 3,
+        list: [
+          {
+            id: 13,
+            academy_id: 3,
+            theme_id: 6,
+            training_id: 7,
+            category: "Test Substansi",
+            all_participant: 0,
+            participant_finished: 0,
+            academy: {
+              id: 3,
+              name: "PRO",
+              created_at: "2021-10-14T00:54:03.000000Z",
+              updated_at: "2021-10-14T00:54:03.000000Z",
+            },
+            theme: {
+              id: 6,
+              academy_id: 3,
+              name: "Get Connected",
+              created_at: "2021-10-14T00:54:15.000000Z",
+              updated_at: "2021-10-14T00:54:15.000000Z",
+            },
+            training: {
+              id: 7,
+              theme_id: 1,
+              name: "Frontend Web React Js",
+              created_at: "2021-10-14T00:54:26.000000Z",
+              updated_at: "2021-10-14T00:54:26.000000Z",
+            },
+          },
+          {
+            id: 22,
+            academy_id: 3,
+            theme_id: 2,
+            training_id: 8,
+            category: "Test Substansi",
+            all_participant: 0,
+            participant_finished: 0,
+            academy: {
+              id: 3,
+              name: "PRO",
+              created_at: "2021-10-14T00:54:03.000000Z",
+              updated_at: "2021-10-14T00:54:03.000000Z",
+            },
+            theme: {
+              id: 2,
+              academy_id: 1,
+              name: "Data Management Staff",
+              created_at: "2021-10-14T00:54:15.000000Z",
+              updated_at: "2021-10-14T00:54:15.000000Z",
+            },
+            training: {
+              id: 8,
+              theme_id: 2,
+              name: "Frontend Web Vue Js",
+              created_at: "2021-10-14T00:54:26.000000Z",
+              updated_at: "2021-10-14T00:54:26.000000Z",
+            },
+          },
+          {
+            id: 17,
+            academy_id: 1,
+            theme_id: 1,
+            training_id: 8,
+            category: "Test Substansi",
+            all_participant: 3,
+            participant_finished: 1,
+            academy: {
+              id: 1,
+              name: "VSGA",
+              created_at: "2021-10-14T00:54:03.000000Z",
+              updated_at: "2021-10-14T00:54:03.000000Z",
+            },
+            theme: {
+              id: 1,
+              academy_id: 1,
+              name: "Cloud Computing Analyst",
+              created_at: "2021-10-14T00:54:15.000000Z",
+              updated_at: "2021-10-14T00:54:15.000000Z",
+            },
+            training: {
+              id: 8,
+              theme_id: 2,
+              name: "Frontend Web Vue Js",
+              created_at: "2021-10-14T00:54:26.000000Z",
+              updated_at: "2021-10-14T00:54:26.000000Z",
+            },
+          },
+        ],
+      },
+      survey: {
+        perPage: 5,
+        total: 9,
+        totalFiltered: 0,
+        list: [],
+      },
+      trivia: {
+        perPage: 5,
+        total: 1,
+        totalFiltered: 1,
+        list: [
+          {
+            id: 17,
+            academy_id: 1,
+            theme_id: 6,
+            training_id: 5,
+            all_participant: 1,
+            participant_finished: 0,
+            academy: {
+              id: 1,
+              name: "VSGA",
+              created_at: "2021-10-14T00:54:03.000000Z",
+              updated_at: "2021-10-14T00:54:03.000000Z",
+            },
+            theme: {
+              id: 6,
+              academy_id: 3,
+              name: "Get Connected",
+              created_at: "2021-10-14T00:54:15.000000Z",
+              updated_at: "2021-10-14T00:54:15.000000Z",
+            },
+            training: {
+              id: 5,
+              theme_id: 5,
+              name: "Web Backend Node Js",
+              created_at: "2021-10-14T00:54:26.000000Z",
+              updated_at: "2021-10-14T00:54:26.000000Z",
+            },
+          },
+        ],
+      },
+      chart: {
+        total_substansi: 5,
+        total_survey: 2,
+        total_trivia: 1,
+        total: 8,
+      },
+    },
+  });
+
+  const data = [];
+
+  Object.entries(dataDummy.data.chart).map((item, index) => {
+    // return console.log(item[0]);
+    return data.push({ name: item[0], value: item[1] });
+  });
+
+  // const data = [
+  //   {
+  //     name: Object.keys(dataDummy.data.chart[0]),
+  //     value: Object.values(dataDummy.data.chart[0]),
+  //   },
+  //   {
+  //     name: Object.keys(dataDummy.data.chart[1]),
+  //     value: Object.values(dataDummy.data.chart[1]),
+  //   },
+  //   {
+  //     name: Object.keys(dataDummy.data.chart[2]),
+  //     value: Object.values(dataDummy.data.chart[2]),
+  //   },
+  // ];
 
   const dummy = [
     {
@@ -240,11 +404,12 @@ const DashbardSubvit = () => {
                 <p className={`${styles.subHeadPeserta}`}>
                   Total Peserta dari Test Substansi, Survey dan TRIVIA
                 </p>
-                <ResponsiveContainer width="100%" height={300}>
+                <ResponsiveContainer height={300}>
                   <PieChart width={500} height={300}>
+                    <Tooltip />
                     <Pie
-                      data={data}
-                      cx={200}
+                      data={data.slice(0, 3)}
+                      cx={250}
                       cy={150}
                       innerRadius={60}
                       outerRadius={80}
@@ -259,18 +424,31 @@ const DashbardSubvit = () => {
                           ></Cell>
                         </>
                       ))}
+                      {/* {Object.values(dataDummy.data.chart).map(
+                        (item, index) => {
+                          console.log(item);
+                          return (
+                            <>
+                              <Cell
+                                key={item}
+                                fill={COLORS[index % COLORS.length]}
+                              ></Cell>
+                            </>
+                          );
+                        }
+                      )} */}
                       <Label
                         width={30}
                         position="center"
                         className={styles.labelChart}
                       >
-                        200k
+                        {dataDummy.data.chart.total}
                       </Label>
                     </Pie>
                   </PieChart>
                 </ResponsiveContainer>
                 <div className="row" style={{ padding: "0px 20px" }}>
-                  <div className="col-sm-4 col-xs-4">
+                  <div className="col-sm-4 ">
                     <div className="row">
                       <div
                         className="col-sm-4 col-xs-4"
@@ -284,7 +462,7 @@ const DashbardSubvit = () => {
                         />
                       </div>
                       <div className={`${styles.substansi} col-sm-8 col-xs-8`}>
-                        100.00
+                        {dataDummy.data.chart.total_substansi}
                         <br />
                         <span className={styles.subTextTotal}>Substansi</span>
                       </div>
@@ -305,7 +483,7 @@ const DashbardSubvit = () => {
                         />
                       </div>
                       <div className={`${styles.survey} col-sm-8 col-xs-8`}>
-                        50.000
+                        {dataDummy.data.chart.total_survey}
                         <br />
                         <span className={styles.subTextTotal}>Survey</span>
                       </div>
@@ -325,7 +503,7 @@ const DashbardSubvit = () => {
                         />
                       </div>
                       <div className={`${styles.trivia} col-sm-8 col-xs-8`}>
-                        50.000
+                        {dataDummy.data.chart.total_trivia}
                         <br />
                         <span className={styles.subTextTotal}>Trivia</span>
                       </div>
