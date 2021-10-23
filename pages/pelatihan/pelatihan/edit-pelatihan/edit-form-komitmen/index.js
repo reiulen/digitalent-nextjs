05fce2f096a519990c472321572ff0704e230fd7
dendyjuliano <dendyjuliano2019@gmail.com>
2021-10-23 22:53:56 +0700

    add middleware admin pelatihan

--- pages/pelatihan/pelatihan/edit-pelatihan/edit-form-komitmen/index.js
+++ pages/pelatihan/pelatihan/edit-pelatihan/edit-form-komitmen/index.js
@@ -1,11 +1,12 @@
-import React, { Suspense } from "react";
+import React from "react";
 
 import dynamic from "next/dynamic";
 import LoadingSkeleton from "../../../../../components/LoadingSkeleton";
-// import EditCommitment from "../../../../../components/content/pelatihan/training/edit/edit-commitment-step3";
 
 import { wrapper } from "../../../../../redux/store";
 import { getSession } from "next-auth/client";
+
+import { middlewareAuthAdminSession } from "../../../../../utils/middleware/authMiddleware";
 
 const EditCommitment = dynamic(
   () =>
@@ -34,10 +35,11 @@ export const getServerSideProps = wrapper.getServerSideProps(
   (store) =>
     async ({ query, req }) => {
       const session = await getSession({ req });
-      if (!session) {
+      const middleware = middlewareAuthAdminSession(session);
+      if (!middleware.status) {
         return {
           redirect: {
-            destination: "http://dts-dev.majapahit.id/login/admin",
+            destination: middleware.redirect,
             permanent: false,
           },
         };
