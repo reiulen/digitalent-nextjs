8005a6571d3b1e222e0e37f9b98a65aa9bfa5233
fandiario <fandi.ario10@gmail.com>
2021-10-24 11:09:14 +0700

    feature/s1_97_kategori-akademi

--- components/wrapper/beranda.wrapper.jsx
+++ components/wrapper/beranda.wrapper.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Head from "next/head";
-// import Footer from "../../user-component/components/beranda/footer"
+
+import NavigationBar from "../../user-component/components/template/Navbar.component"
+import Footer from "../../user-component/components/beranda/footer"
+
 import "../../styles/beranda.module.css"
 
 const BerandaWrapper = ({ children, title }) => {
@@ -10,7 +13,9 @@ const BerandaWrapper = ({ children, title }) => {
                 <title>{title}</title>
             </Head>
             <div>
+                <NavigationBar />
                 {children}
+            
             </div>
 
             {/* <Footer /> */}
