48a7313a79be273b54d68e6fd406d22607562991
dendyjuliano <dendyjuliano2019@gmail.com>
2021-10-24 13:24:54 +0700

    save change

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import { getSession } from "next-auth/client";
 
 // import LoginAdmin from "../components/content/auth/admin/login";
-import Beranda from "../user-component/content/beranda/beranda"
+import Beranda from "../user-component/content/beranda/beranda";
 
 import { middlewareAuthAdminSession } from "../utils/middleware/authMiddleware";
 
 import { wrapper } from "../redux/store";
 import { getAllAkademi } from "../redux/actions/beranda/beranda.actions";
 import { getTemaByAkademi } from "../redux/actions/beranda/beranda.actions";
-import { getAllPublikasi } from "../redux/actions/beranda/beranda.actions"
+import { getAllPublikasi } from "../redux/actions/beranda/beranda.actions";
 // import { getPelatihanByTema } from "../redux/actions/beranda/beranda.actions";
 
 export default function HomePage() {
@@ -22,38 +22,37 @@ export default function HomePage() {
   );
 }
 
-export const getStaticProps = wrapper.getStaticProps((store) => async({ query, req }) => {
-  const session = await getSession({ req });
-  // const middleware = middlewareAuthAdminSession(session);
-  // if (!middleware.status) {
-  //   return {
-  //     redirect: {
-  //       destination: middleware.redirect,
-  //       permanent: false,
-  //     },
-  //   };
-  // }
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ query, req }) => {
+      const session = await getSession({ req });
+      // const middleware = middlewareAuthAdminSession(session);
+      // if (!middleware.status) {
+      //   return {
+      //     redirect: {
+      //       destination: middleware.redirect,
+      //       permanent: false,
+      //     },
+      //   };
+      // }
 
-  await store.dispatch(
-    getAllAkademi()
-  );
+      console.log(session);
 
-  await store.dispatch (
-    getTemaByAkademi()
-  )
+      await store.dispatch(getAllAkademi());
 
-  await store.dispatch (
-    getAllPublikasi()
-  )
+      await store.dispatch(getTemaByAkademi());
 
-  // await store.dispatch (
-  //   getPelatihanByTema()
-  // )
+      await store.dispatch(getAllPublikasi());
 
-  return {
-    props: {
-      data: "auth",
-      session,
-    },
-  };
-})
+      // await store.dispatch (
+      //   getPelatihanByTema()
+      // )
+
+      return {
+        props: {
+          data: "auth",
+          session,
+        },
+      };
+    }
+);
