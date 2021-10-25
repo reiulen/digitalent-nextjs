d904ddb5328c0c8c4a77fb327204ef9a8c80308e
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2021-10-25 11:28:29 +0700

    detail pelatihan fix

--- user-component/content/detail/kategori/pelatihan/detail-pelatihan.jsx
+++ user-component/content/detail/kategori/pelatihan/detail-pelatihan.jsx
@@ -25,7 +25,7 @@ const DetailPelatihan = () => {
 
       {pelatihan ? (
         <div className="row mt-13">
-          <div className="col-12 col-md-8">
+          <div className="col-12 col-xl-8">
             <div className="bg-white">
               <div className="ml-2 mb-3">
                 <h1 className="fw-700 fz-36">{pelatihan.name}</h1>
@@ -35,7 +35,7 @@ const DetailPelatihan = () => {
                 </div>
 
                 <div className="row mt-8">
-                  <div className="col-3">
+                  <div className="col-6 col-sm-5">
                     <div>
                       <p className="mb-2" style={{ color: "#6C6C6C" }}>
                         Registrasi
@@ -51,7 +51,7 @@ const DetailPelatihan = () => {
                       </p>
                     </div>
                   </div>
-                  <div className="col-3">
+                  <div className="col-6 col-sm-2">
                     <div>
                       <p className="mb-2" style={{ color: "#6C6C6C" }}>
                         Pelaksanaan
@@ -59,7 +59,7 @@ const DetailPelatihan = () => {
                       <p className="fz-16">{pelatihan.metode_pelatihan}</p>
                     </div>
                   </div>
-                  <div className="col-3">
+                  <div className="col-6 col-sm-3">
                     <div>
                       <p className="mb-2" style={{ color: "#6C6C6C" }}>
                         Pendaftar
@@ -67,7 +67,7 @@ const DetailPelatihan = () => {
                       <p className="fz-16">{pelatihan.kuota_peserta} Peserta</p>
                     </div>
                   </div>
-                  <div className="col-3">
+                  <div className="col-6 col-sm-2">
                     <div className="d-flex align-items-center justify-content-end">
                       <button className="btn btn-white roundedss-border mr-4">
                         {/* <i className="ri-share-line"></i> */}
@@ -102,53 +102,39 @@ const DetailPelatihan = () => {
             </div>
           </div>
 
-          <div className="col-12 col-md-4">
+          <div className="col-12 col-xl-4">
             <div className="bg-white border rounded p-6">
-   
-                <h4 className="fz-20 fw-600">Ikuti Pelatihan</h4>
-                <span className="fz-16">
-                  {moment(pelatihan.pelatihan_mulai).format("DD MMMM YYYY")} -{" "}
-                  {moment(pelatihan.pelatihan_selesai).format("DD MMMM YYYY")}
-                </span>
+              <h4 className="fz-20 fw-600">Ikuti Pelatihan</h4>
+              <span className="fz-16">
+                {moment(pelatihan.pelatihan_mulai).format("DD MMMM YYYY")} -{" "}
+                {moment(pelatihan.pelatihan_selesai).format("DD MMMM YYYY")}
+              </span>
 
-                {pelatihan.status === "Closed" ? (
-      
-                    <Link
-                      href={`/peserta/form-pendaftaran?id=${pelatihan.id}`}
-                      passHref
-                    >
-                      <a>
-                        <button className="btn btn-primary rounded-pill btn-block mt-10  py-4">
-                          Daftar Pelatihan
-                        </button>
-                      </a>
-                    </Link>
-          
-                ) : null}
+              {pelatihan.status === "Closed" ? (
+                <Link
+                  href={`/peserta/form-pendaftaran?id=${pelatihan.id}`}
+                  passHref
+                >
+                  <a>
+                    <button className="btn btn-primary rounded-pill btn-block mt-10  py-4">
+                      Daftar Pelatihan
+                    </button>
+                  </a>
+                </Link>
+              ) : null}
 
-                <button className="btn btn-outline-primary-new mt-4 rounded-pill mb-8 btn-block py-4">
-                  <i className="ri-download-cloud-fill"></i>
-                  <span>Unduh Silabus</span>
-                </button>
-        
+              <button className="btn btn-outline-primary-new mt-4 rounded-pill mb-8 btn-block py-4">
+                <i className="ri-download-cloud-fill"></i>
+                <span>Unduh Silabus</span>
+              </button>
 
-              {/* Border */}
-              {/* <div
-                className="row ml-3 my-5 mr-5"
-                style={{
-                  height: "2px",
-                  backgroundColor: "#ADB5BD",
-                  opacity: "0.4",
-                }}
-              ></div> */}
               <hr />
               <div className="d-flex flex-wrap align-items-start mt-10">
-                
-                  <Image
-                    src={`/assets/icon/alamat-1.svg`}
-                    width={30}
-                    height={30}
-                  />
+                <Image
+                  src={`/assets/icon/alamat-1.svg`}
+                  width={30}
+                  height={30}
+                />
                 <div className="ml-4">
                   <p className="fw-600 fz-18 mb-2">Alamat</p>
                   <p className="fz-16">{pelatihan.alamat}</p>
@@ -156,91 +142,46 @@ const DetailPelatihan = () => {
               </div>
 
               <div className="d-flex flex-wrap align-items-start mt-4">
-                
-                  <Image
-                    src={`/assets/icon/jam-1.svg`}
-                    width={30}
-                    height={30}
-                  />
+                <Image src={`/assets/icon/jam-1.svg`} width={30} height={30} />
                 <div className="ml-4">
                   <p className="fw-600 fz-18 mb-2">Jadwal Pelatihan</p>
-                  <p className="fz-16">{moment(pelatihan.pelatihan_mulai).format("DD MMMM YYYY")} -{" "}
-                    {moment(pelatihan.pelatihan_selesai).format("DD MMMM YYYY")}</p>
+                  <p className="fz-16">
+                    {moment(pelatihan.pelatihan_mulai).format("DD MMMM YYYY")} -{" "}
+                    {moment(pelatihan.pelatihan_selesai).format("DD MMMM YYYY")}
+                  </p>
                 </div>
               </div>
 
               <div className="d-flex flex-wrap align-items-start mt-4">
-                
-                  <Image
-                    src={`/assets/icon/jam-1.svg`}
-                    width={30}
-                    height={30}
-                  />
+                <Image src={`/assets/icon/jam-1.svg`} width={30} height={30} />
                 <div className="ml-4">
                   <p className="fw-600 fz-18 mb-2">Jadwal Pelatihan</p>
-                  <p className="fz-16">{moment(pelatihan.pelatihan_mulai).format("DD MMMM YYYY")} -{" "}
-                    {moment(pelatihan.pelatihan_selesai).format("DD MMMM YYYY")}</p>
+                  <p className="fz-16">
+                    {moment(pelatihan.pelatihan_mulai).format("DD MMMM YYYY")} -{" "}
+                    {moment(pelatihan.pelatihan_selesai).format("DD MMMM YYYY")}
+                  </p>
                 </div>
               </div>
 
-
               <div className="d-flex flex-wrap align-items-start mt-4">
-                
-                  <Image
-                    src={`/assets/icon/kuota-1.svg`}
-                    width={30}
-                    height={30}
-                  />
+                <Image
+                  src={`/assets/icon/kuota-1.svg`}
+                  width={30}
+                  height={30}
+                />
                 <div className="ml-4">
                   <p className="fw-600 fz-18 mb-2">Kuota</p>
                   <p className="fz-16">{pelatihan.kuota_pendaftar} peserta</p>
                 </div>
               </div>
-
-               {/* <div className="row mt-5 p-3">
-                <div className="col-2">
-                  <Image
-                    src={`/assets/icon/kuota-1.svg`}
-                    width={50}
-                    height={50}
-                  />
-                </div>
-                <div className="col-10">
-                  <div className="font-weight-bolder">Kuota</div>
-                  <div>{pelatihan.kuota_pendaftar} orang</div>
-                </div>
-              </div> */}
-
-              {/* <div className="row mt-5 p-3">
-                <div className="col-2">
-                  <Image
-                    src={`/assets/icon/jam-1.svg`}
-                    width={50}
-                    height={50}
-                  />
-                </div>
-                <div className="col-10">
-                  <div className="font-weight-bolder">Jadwal Pelatihan</div>
-                  <div>
-                    {moment(pelatihan.pelatihan_mulai).format("DD MMMM YYYY")} -{" "}
-                    {moment(pelatihan.pelatihan_selesai).format("DD MMMM YYYY")}
-                  </div>
-                </div>
-              </div> */}
-
-             
-
-
-
             </div>
 
-            <div className="bg-white border rounded mt-5">
-              <div className="row">
-                <h4 className="font-weight-bolder ml-5 p-3">Mitra Pelatihan</h4>
-              </div>
+            <div className="bg-white border rounded mt-5 p-6">
+                <h4 className="fz-20 fw-600">Mitra Pelatihan</h4>
+             
 
-              <div className="row mb-3">
-                <div className="col-3 ml-3">
+              <div className="d-flex align-items-center mt-6">
+                <div>
                   <Image
                     src={`/assets/icon/kuota-1.svg`}
                     // src={`https://dts-beasiswa-dev.s3-ap-southeast-1.amazonaws.com/${pelatihan.gambar_mitra}`}
@@ -248,12 +189,17 @@ const DetailPelatihan = () => {
                     height={50}
                   />
                 </div>
-                <div className="col-5">
-                  <div className="font-weight-bolder">
+
+
+                <div className="ml-6">
+                  <p className="fw-600 fz-16 mb-2">
                     Mitra Nama {pelatihan.mitra_nama}
-                  </div>
-                  <div className="text-muted">Mitra {pelatihan.mitra}</div>
+                  </p>
+                  <p style={{color:"#6C6C6C"}}>Mitra {pelatihan.mitra}</p>
                 </div>
+
+
+
               </div>
             </div>
 
