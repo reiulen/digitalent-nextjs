3c8fc81b6fe7c875d5baf741b7ed366b3d0cc52f
dendyjuliano <dendyjuliano2019@gmail.com>
2021-10-28 01:31:27 +0700

    save change

--- pages/peserta/done-substansi/index.js
+++ pages/peserta/done-substansi/index.js
@@ -1,9 +1,14 @@
+import React, { useEffect } from "react";
+
 import { getSession } from "next-auth/client";
 
 import LoadingSkeleton from "../../../components/LoadingSkeleton";
 import dynamic from "next/dynamic";
+import { useSelector, useDispatch } from "react-redux";
 import { wrapper } from "../../../redux/store";
 import { getDataPribadi } from "../../../redux/actions/pelatihan/function.actions";
+import { getDashboardPeserta } from "../../../redux/actions/pelatihan/dashboard-peserta.actions";
+import { getPelatihan } from "../../../redux/actions/pelatihan/register-training.actions";
 import { middlewareAuthPesertaSession } from "../../../utils/middleware/authMiddleware";
 
 const Done = dynamic(() => import("../../../user-component/content/done"), {
@@ -18,7 +23,21 @@ const Layout = dynamic(() =>
 );
 
 export default function SubvitDone(props) {
+  const dispatch = useDispatch();
   const session = props.session.user.user.data.user;
+
+  const { error: errorDashboard, dataDashboard } = useSelector(
+    (state) => state.dashboardPeserta
+  );
+
+  const { count, pelatihan, subvit } = dataDashboard;
+
+  useEffect(() => {
+    if (pelatihan) {
+      dispatch(getPelatihan(session.token, pelatihan.pelatihan_berjalan.id));
+    }
+  }, [pelatihan, session]);
+
   return (
     <>
       <div className="d-flex flex-column flex-root">
@@ -46,6 +65,9 @@ export const getServerSideProps = wrapper.getServerSideProps(
         };
       }
       await store.dispatch(getDataPribadi(session?.user.user.data.user.token));
+      await store.dispatch(
+        getDashboardPeserta(session?.user.user.data.user.token)
+      );
 
       return {
         props: { data: "auth", session, title: "Selesai Test Substansi" },
