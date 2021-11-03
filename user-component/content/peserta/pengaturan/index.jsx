0e054088e1f7a4945b4348f167996c39c2ee05c1
firawz <ahmadfiraz@majapahit.id>
2021-11-03 18:04:19 +0700

    modifying things

--- user-component/content/peserta/pengaturan/index.jsx
+++ user-component/content/peserta/pengaturan/index.jsx
@@ -13,16 +13,36 @@ export default function SeleksiAdministrasi({ session }) {
   const { error: errorDataPribadi, dataPribadi } = useSelector(
     (state) => state.getDataPribadi
   );
+
+  //   START PASSWORD
+  const [password, setPassword] = useState();
+  const [showUbahPasswordModal, setShowUbahPasswordModal] = useState(false);
+  const handleClosePasswordModal = () => setShowUbahPasswordModal(false);
+  const handleShowUbahPasswordModal = () => setShowUbahPasswordModal(true);
+
+  const [hidePassword, setHidePassword] = useState(true);
+
+  const handlerShowPassword = (value) => {
+    setHidePassword(value);
+    // var input = document.getElementById("input-password");
+    // if (input.type === "password") {
+    //   input.type = "text";
+    // } else {
+    //   input.type = "password";
+    // }
+  };
+  // END PASSWORD
+
+  //   START EMAIL
   const [email, setEmail] = useState("");
   const [showUbahEmailModal, setShowUbahEmailModal] = useState(false);
-  const handleClose = () => setShowUbahEmailModal(false);
+  const handleCloseEmailModal = () => setShowUbahEmailModal(false);
   const handleShowUbahEmail = () => setShowUbahEmailModal(true);
-
-  const [otp, setOtp] = useState();
-
+  const [otpEmail, setOtpEmail] = useState();
   const [showUbahEmailModalOtp, setShowUbahEmailModalOtp] = useState(false);
-  const handleCloseOtp = () => setShowUbahEmailModalOtp(false);
+  const handleCloseEmailOtp = () => setShowUbahEmailModalOtp(false);
   const handleShowUbahEmailOtp = () => setShowUbahEmailModalOtp(true);
+  // END EMAIL
   const simpleValidator = useRef(
     new SimpleReactValidator({
       locale: "id",
@@ -32,15 +52,6 @@ export default function SeleksiAdministrasi({ session }) {
       },
     })
   );
-  const handleOtpEmail = (el, i) => {
-    if (isNaN(el.value)) return false;
-    setOtp([...otp.map((d, idx) => (idx === i ? el.value : d))]);
-
-    //Focus next input
-    if (el.nextSibling) {
-      el.nextSibling.focus();
-    }
-  };
 
   const handlePostUbahEmail = () => {
     handleShowUbahEmailOtp();
@@ -52,7 +63,7 @@ export default function SeleksiAdministrasi({ session }) {
     }
   };
   const [showResendButton, setShowResendButton] = useState(false);
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(30);
   useEffect(() => {
     if (showUbahEmailModalOtp) {
       if (count > 0) {
@@ -70,10 +81,7 @@ export default function SeleksiAdministrasi({ session }) {
   return (
     <PesertaWrapper>
       <Col lg={12} className="px-0">
-        <Card
-          className="card-custom card-stretch gutter-b p-0"
-          style={{ height: "602px" }}
-        >
+        <Card className="card-custom card-stretch gutter-b p-0">
           <Card.Body className="p-10">
             <div>
               <h1 className="font-weight-bolder">Kontak</h1>
@@ -99,12 +107,87 @@ export default function SeleksiAdministrasi({ session }) {
               </div>
               <div className="mt-5">
                 {dataPribadi.email || "-"}
+                {dataPribadi.email_verifikasi == true && (
+                  <span className="ml-5">
+                    <button
+                      className={`text-primary ${style.btn_ubah}`}
+                      onClick={handleShowUbahEmail}
+                    >
+                      Ubah
+                    </button>
+                  </span>
+                )}
+              </div>
+              {!dataPribadi.email_verifikasi && (
+                <Button
+                  variant="danger"
+                  className="rounded-full mt-5 py-3 px-10"
+                >
+                  Verifikasi
+                </Button>
+              )}
+            </div>
+            <div className="mb-10">
+              <br />
+              <div
+                className={`d-flex align-item-center ${style.text14}`}
+                style={{ height: "24px" }}
+              >
+                Nomor Handphone
+                {dataPribadi.handphone_verifikasi ? (
+                  <div
+                    className={`rounded-circle d-flex align-items-center justify-content-center mx-5 ${style.iconBackgroundSuccess}`}
+                  >
+                    <i class="ri-check-fill" style={{ color: "#00996A" }}></i>
+                  </div>
+                ) : (
+                  <div
+                    className={`rounded-full p-4 d-flex align-items-center justify-content-center mx-5 ${style.iconBackgroundFail}`}
+                  >
+                    Unverified
+                  </div>
+                )}
+              </div>
+              <div className="mt-5">
+                {dataPribadi.nomor_handphone || "-"}
+                {dataPribadi.handphone_verifikasi == true && (
+                  <span className="ml-5">
+                    <button
+                      className={`text-primary ${style.btn_ubah}`}
+                      onClick={handleShowUbahEmail}
+                    >
+                      Ubah
+                    </button>
+                  </span>
+                )}
+              </div>
+              {dataPribadi.handphone_verifikasi == false && (
+                <Button
+                  variant="danger"
+                  className="rounded-full mt-5 py-3 px-10"
+                >
+                  Verifikasi
+                </Button>
+              )}
+            </div>
+            <hr />
+            <div className="mt-10">
+              <h1 className="font-weight-bolder">Kontak</h1>
+              <br />
+              <div
+                className={`d-flex align-item-center ${style.text14}`}
+                style={{ height: "24px" }}
+              >
+                Password
+              </div>
+              <div className="mt-5">
+                ***********
                 <span className="ml-5">
                   <button
                     className={`text-primary ${style.btn_ubah}`}
-                    onClick={handleShowUbahEmail}
+                    onClick={handleShowUbahPasswordModal}
                   >
-                    Ubah
+                    Ubah Password
                   </button>
                 </span>
               </div>
@@ -112,11 +195,10 @@ export default function SeleksiAdministrasi({ session }) {
           </Card.Body>
         </Card>
       </Col>
-      {/* Modal Email */}
-
+      {/* STAR MODAL PASSWORD */}
       <Modal
         show={showUbahEmailModal}
-        onHide={handleClose}
+        onHide={handleCloseEmailModal}
         backdrop="static"
         keyboard={false}
         centered
@@ -125,7 +207,7 @@ export default function SeleksiAdministrasi({ session }) {
           <Modal.Title>Ubah Email</Modal.Title>
           <Modal.Title
             className="d-flex align-items-center"
-            onClick={handleClose}
+            onClick={handleCloseEmailModal}
           >
             <button className={`${style.btn_ubah}`}>
               <i class="ri-close-fill"></i>
@@ -182,7 +264,7 @@ export default function SeleksiAdministrasi({ session }) {
               style={{ fontSize: "14px" }}
               onClick={() => {
                 handlePostUbahEmail();
-                handleClose();
+                handleCloseEmailModal();
               }}
             >
               Lanjut
@@ -190,10 +272,11 @@ export default function SeleksiAdministrasi({ session }) {
           </div>
         </Modal.Body>
       </Modal>
-
+      {/* END MODAL EMAIL */}
+      {/* START MODAL EMAIL OTP */}
       <Modal
         show={showUbahEmailModalOtp}
-        onHide={handleCloseOtp}
+        onHide={handleCloseEmailOtp}
         backdrop="static"
         keyboard={false}
         centered
@@ -203,7 +286,7 @@ export default function SeleksiAdministrasi({ session }) {
           <Modal.Title>Verifikasi Kontak</Modal.Title>
           <Modal.Title
             className="d-flex align-items-center"
-            onClick={handleCloseOtp}
+            onClick={handleCloseEmailOtp}
           >
             <button className={`${style.btn_ubah}`}>
               <i class="ri-close-fill"></i>
@@ -222,8 +305,8 @@ export default function SeleksiAdministrasi({ session }) {
           </div>
           <div>
             <OtpInput
-              value={otp}
-              onChange={(e) => setOtp(e)}
+              value={otpEmail}
+              onChange={(e) => setOtpEmail(e)}
               numInputs={6}
               inputStyle="w-100 p-4 mx-5 my-10 form-control"
               shouldAutoFocus
@@ -267,7 +350,132 @@ export default function SeleksiAdministrasi({ session }) {
           </div>
         </Modal.Body>
       </Modal>
-      {/* Modal Email Selesai */}
+      {/* END MODAL EMAIL OTP */}
+      <Modal
+        show={showUbahPasswordModal}
+        onHide={handleClosePasswordModal}
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title>Ubah Password</Modal.Title>
+          <Modal.Title
+            className="d-flex align-items-center"
+            onClick={handleClosePasswordModal}
+          >
+            <button className={`${style.btn_ubah}`}>
+              <i class="ri-close-fill"></i>
+            </button>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label
+                className="mb-3"
+                style={{ fontSize: "16px", color: "#1F1F1F" }}
+              >
+                Password Lama
+              </Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter email"
+                style={{ fontSize: "14px" }}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label
+                className="mb-3"
+                style={{ fontSize: "16px", color: "#1F1F1F" }}
+              >
+                Password Baru
+              </Form.Label>
+              <Form.Control
+                style={{ fontSize: "14px" }}
+                type="email"
+                placeholder="Masukan Email Baru"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label
+                className="mb-3"
+                style={{ fontSize: "16px", color: "#1F1F1F" }}
+              >
+                Password Baru
+              </Form.Label>
+              <Form.Control
+                style={{ fontSize: "14px" }}
+                type="email"
+                placeholder="Masukan Email Baru"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </Form.Group>
+            <div className="form-group">
+              <label className="form-auth-label">Password</label>
+              <div className="position-relative">
+                <input
+                  id="input-password"
+                  //   type="password"
+                  type={hidePassword ? "password" : "text"}
+                  className="form-control form-control-auth pr-10"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Masukkan Password"
+                  onBlur={() =>
+                    simpleValidator.current.showMessageFor("Password")
+                  }
+                />
+                {hidePassword === true ? (
+                  <i
+                    className="ri-eye-fill right-center-absolute cursor-pointer"
+                    style={{ right: "10px" }}
+                    onClick={() => {
+                      setHidePassword(!hidePassword);
+                      //   handlerShowPassword(false);
+                    }}
+                  />
+                ) : (
+                  <i
+                    className="ri-eye-off-fill right-center-absolute cursor-pointer"
+                    style={{ right: "10px" }}
+                    onClick={() => {
+                      setHidePassword(!hidePassword);
+                      //   handlerShowPassword(false);
+                    }}
+                  />
+                )}
+              </div>
+              {simpleValidator.current.message(
+                "Password",
+                password,
+                "required",
+                {
+                  className: "text-danger",
+                }
+              )}
+            </div>
+          </Form>
+          <div className="d-flex justify-content-end mt-14">
+            <Button
+              variant="primary"
+              className="rounded-full py-4 px-8"
+              style={{ fontSize: "14px" }}
+              onClick={() => {
+                handlePostUbahEmail();
+                handleCloseEmailModal();
+              }}
+            >
+              Lanjut
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </PesertaWrapper>
   );
 }
