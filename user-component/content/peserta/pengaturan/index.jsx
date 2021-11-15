6d19513e930a06c082f3296fed1e479bebe8206c
Ahmad Firaz <ahmadfiraz@gmail.com>
2021-11-15 22:07:16 +0700

    hotfix infinite loop navbar and pengaturan

--- user-component/content/peserta/pengaturan/index.jsx
+++ user-component/content/peserta/pengaturan/index.jsx
@@ -5,17 +5,17 @@ import PesertaWrapper from "../../../components/wrapper/Peserta.wrapper";
 import style from "./style.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import SimpleReactValidator from "simple-react-validator";
-// import OtpInput from "react-otp-input";
+import OtpInput from "react-otp-input";
 import axios from "axios";
 import { toast } from "react-toastify";
 import Swal from "sweetalert2";
 import { getDataPribadi } from "../../../../redux/actions/pelatihan/function.actions";
 
-import OtpInput from "react-otpcode-input";
+// import OtpInput from "react-otpcode-input";
 
 export default function Pengaturan({ session }) {
   const { error: errorDataPribadi, dataPribadi } = useSelector(
-    (state) => state.getDataPribadi
+    state => state.getDataPribadi
   );
   const dispatch = useDispatch();
   const config = {
@@ -59,7 +59,7 @@ export default function Pengaturan({ session }) {
   const handleCloseEmailOtp = () => setShowUbahEmailModalOtp(false);
   const handleShowUbahEmailOtp = () => setShowUbahEmailModalOtp(true);
 
-  const notify = (value) =>
+  const notify = value =>
     toast.error(`${value}`, {
       position: "top-right",
       autoClose: 5000,
@@ -71,7 +71,7 @@ export default function Pengaturan({ session }) {
     });
 
   // POST LANJUT UBAH HANDPHONE
-  const handleLanjutUbahHandphone = async (nomor_hp) => {
+  const handleLanjutUbahHandphone = async nomor_hp => {
     const body = {
       old_nomor_hp: dataPribadi.nomor_handphone,
       nomor_hp,
@@ -102,7 +102,7 @@ export default function Pengaturan({ session }) {
     }
   };
   // POST OTP UBAH HANDPHONE
-  const handlePostOtpUbahHandphone = async (token) => {
+  const handlePostOtpUbahHandphone = async token => {
     const body = {
       old_nomor_hp: dataPribadi.nomor_handphone,
       nomor_hp: handphone,
@@ -177,7 +177,7 @@ export default function Pengaturan({ session }) {
   );
 
   //POST EMAIL LANJUT
-  const handleLanjutUbahEmail = async (email) => {
+  const handleLanjutUbahEmail = async email => {
     const body = {
       old_email: dataPribadi.email,
       email,
@@ -277,7 +277,7 @@ export default function Pengaturan({ session }) {
     }
   };
   // POST OTP VERIFIKASI EMAIL
-  const handlePostOtpEmailVerifikasi = async (token) => {
+  const handlePostOtpEmailVerifikasi = async token => {
     const body = {
       email: dataPribadi.email,
       token,
@@ -304,7 +304,7 @@ export default function Pengaturan({ session }) {
     }
   };
   // POST OTP VERIFIKASI HP
-  const handlePostOtpHpVerifikasi = async (token) => {
+  const handlePostOtpHpVerifikasi = async token => {
     const body = {
       email: dataPribadi.email,
       token,
@@ -336,7 +336,7 @@ export default function Pengaturan({ session }) {
     if (showUbahEmailModalOtp) {
       if (count > 0) {
         const secondsLeft = setInterval(() => {
-          setCount((c) => c - 1);
+          setCount(c => c - 1);
         }, 1000);
         return () => clearInterval(secondsLeft);
       }
@@ -361,7 +361,10 @@ export default function Pengaturan({ session }) {
                   <div
                     className={`rounded-circle d-flex align-items-center justify-content-center mx-5 ${style.iconBackgroundSuccess}`}
                   >
-                    <i class="ri-check-fill" style={{ color: "#00996A" }}></i>
+                    <i
+                      className="ri-check-fill"
+                      style={{ color: "#00996A" }}
+                    ></i>
                   </div>
                 ) : (
                   <div
@@ -408,7 +411,10 @@ export default function Pengaturan({ session }) {
                   <div
                     className={`rounded-circle d-flex align-items-center justify-content-center mx-5 ${style.iconBackgroundSuccess}`}
                   >
-                    <i class="ri-check-fill" style={{ color: "#00996A" }}></i>
+                    <i
+                      className="ri-check-fill"
+                      style={{ color: "#00996A" }}
+                    ></i>
                   </div>
                 ) : (
                   <div
@@ -484,7 +490,7 @@ export default function Pengaturan({ session }) {
             onClick={handleCloseEmailModal}
           >
             <button className={`${style.btn_ubah}`}>
-              <i class="ri-close-fill"></i>
+              <i className="ri-close-fill"></i>
             </button>
           </Modal.Title>
         </Modal.Header>
@@ -516,7 +522,7 @@ export default function Pengaturan({ session }) {
                 style={{ fontSize: "14px" }}
                 type="email"
                 placeholder="Masukkan Email Baru"
-                onChange={(e) => {
+                onChange={e => {
                   setEmail(e.target.value);
                 }}
               />
@@ -562,7 +568,7 @@ export default function Pengaturan({ session }) {
             onClick={handleCloseEmailOtp}
           >
             <button className={`${style.btn_ubah}`}>
-              <i class="ri-close-fill"></i>
+              <i className="ri-close-fill"></i>
             </button>
           </Modal.Title>
         </Modal.Header>
@@ -593,7 +599,7 @@ export default function Pengaturan({ session }) {
           <div>
             <OtpInput
               value={otpEmail}
-              onChange={(e) => setOtpEmail(e)}
+              onChange={e => setOtpEmail(e)}
               numInputs={6}
               inputStyle="w-100 p-4 mx-5 my-10 form-control"
               shouldAutoFocus
@@ -670,7 +676,7 @@ export default function Pengaturan({ session }) {
             onClick={handleClosePasswordModal}
           >
             <button className={`${style.btn_ubah}`}>
-              <i class="ri-close-fill"></i>
+              <i className="ri-close-fill"></i>
             </button>
           </Modal.Title>
         </Modal.Header>
@@ -689,7 +695,7 @@ export default function Pengaturan({ session }) {
                   type={hidePasswordLama ? "password" : "text"}
                   className="form-control form-control-auth pr-10"
                   value={passwordLama}
-                  onChange={(e) => setPasswordLama(e.target.value)}
+                  onChange={e => setPasswordLama(e.target.value)}
                   placeholder="Masukkan Password Anda"
                   // onBlur={() =>
                   //   simpleValidator.current.showMessageFor("Password")
@@ -738,7 +744,7 @@ export default function Pengaturan({ session }) {
                   type={hidePasswordBaru ? "password" : "text"}
                   className="form-control form-control-auth pr-10"
                   value={passwordBaru}
-                  onChange={(e) => setPasswordBaru(e.target.value)}
+                  onChange={e => setPasswordBaru(e.target.value)}
                   placeholder="Masukkan Password Anda"
                 />
                 {hidePasswordBaru === true ? (
@@ -786,7 +792,7 @@ export default function Pengaturan({ session }) {
                   type={hidePasswordBaru2 ? "password" : "text"}
                   className="form-control form-control-auth pr-10"
                   value={passwordBaru2}
-                  onChange={(e) => setPasswordBaru2(e.target.value)}
+                  onChange={e => setPasswordBaru2(e.target.value)}
                   placeholder="Masukkan Password Anda"
                 />
                 {hidePasswordBaru2 === true ? (
@@ -862,7 +868,7 @@ export default function Pengaturan({ session }) {
             onClick={handleCloseHandphoneModal}
           >
             <button className={`${style.btn_ubah}`}>
-              <i class="ri-close-fill"></i>
+              <i className="ri-close-fill"></i>
             </button>
           </Modal.Title>
         </Modal.Header>
@@ -894,7 +900,7 @@ export default function Pengaturan({ session }) {
                 style={{ fontSize: "14px" }}
                 type="email"
                 placeholder="Masukkan No HP Baru"
-                onChange={(e) => {
+                onChange={e => {
                   setHandphone(e.target.value);
                 }}
               />
