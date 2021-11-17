153fd2658f5b296e4bc0e6ee6b1025df7f90ef22
rifky1211 <rifky363@gmail.com>
2021-11-17 15:08:09 +0700

    add fitur view list peserta di pelatihan

--- pages/peserta/artikel/preview/[id].js
+++ pages/peserta/artikel/preview/[id].js
@@ -1,16 +1,16 @@
 import dynamic from "next/dynamic";
 import { getSession } from "next-auth/client";
-import { middlewareAuthAdminSession } from "../../../utils/middleware/authMiddleware";
+import { middlewareAuthAdminSession } from "../../../../utils/middleware/authMiddleware";
 
-import { getDataPribadi } from "../../../redux/actions/pelatihan/function.actions";
-import { getDashboardPeserta } from "../../../redux/actions/pelatihan/dashboard-peserta.actions";
-import LoadingContent from "../../../user-component/content/peserta/components/loader/LoadingContent";
+import { getDataPribadi } from "../../../../redux/actions/pelatihan/function.actions";
+import { getDashboardPeserta } from "../../../../redux/actions/pelatihan/dashboard-peserta.actions";
+import LoadingContent from "../../../../user-component/content/peserta/components/loader/LoadingContent";
 import { useRouter } from "next/router";
-import { getAllAkademi } from "../../../redux/actions/beranda/beranda.actions";
-import { wrapper } from "../../../redux/store";
+import { getAllAkademi } from "../../../../redux/actions/beranda/beranda.actions";
+import { wrapper } from "../../../../redux/store";
 
 const Preview = dynamic(
-    () => import("../../../user-component/content/peserta/artikel/preview"),
+    () => import("../../../../user-component/content/peserta/artikel/preview"),
     {
         loading: function loadingNow() {
             return <LoadingContent />
@@ -20,7 +20,7 @@ const Preview = dynamic(
 );
 
 const Layout = dynamic(
-    () => import("../../../user-component/components/template/Layout.component")
+    () => import("../../../../user-component/components/template/Layout.component")
 );
 
 export default function TambahArtikel(props) {
