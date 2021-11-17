ba02f0217da9ab1c0a3138f1da498fd8106589bb
dendy.j.mjph <dendyjuliano2019@gmail.com>
2021-11-17 07:50:32 +0700

    save change

--- user-component/content/beranda/penyelenggara.jsx
+++ user-component/content/beranda/penyelenggara.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, InputGroup, Form, Button } from "react-bootstrap";
 import SubHeaderComponent from "../../components/template/Subheader.component";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Pagination from "react-js-pagination";
+import { getAllPenyeleggaraPage } from "../../../redux/actions/beranda/beranda.actions";
+import { SweatAlert } from "../../../utils/middleware/helper/index";
+import PulseLoaderRender from "../../components/loader/PulseLoader";
 
 const Penyelenggara = () => {
   const dispatch = useDispatch();
@@ -13,6 +16,32 @@ const Penyelenggara = () => {
   const { penyelenggara, loading, error } = useSelector(
     (state) => state.allPenyelenggaraPage
   );
+
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [order, setOrder] = useState("asc");
+
+  useEffect(() => {
+    if (error) {
+      SweatAlert("Error", error, "error");
+    }
+  }, [error]);
+
+  const handlePagination = (pageNumber) => {
+    setPage(pageNumber);
+    dispatch(getAllPenyeleggaraPage(pageNumber, 24, search, order));
+  };
+
+  const handleSearch = () => {
+    setPage(1);
+    dispatch(getAllPenyeleggaraPage(1, 24, search, order));
+  };
+
+  const handleOrder = (value) => {
+    setPage(1);
+    setOrder(value);
+    dispatch(getAllPenyeleggaraPage(1, 24, search, order));
+  };
 
   return (
     <>
@@ -52,6 +81,8 @@ const Penyelenggara = () => {
                       className="form-control border-left-0 border p-0 bg-light"
                       placeholder="Cari Penyelenggara.."
                       style={{ height: "46px" }}
+                      onChange={(e) => setSearch(e.target.value)}
+                      value={search}
                     />
                     <div>
                       <button
@@ -61,7 +92,8 @@ const Penyelenggara = () => {
                           borderBottomRightRadius: "150px",
                           height: "46px",
                         }}
-                        type="submit"
+                        type="button"
+                        onClick={() => handleSearch()}
                       >
                         Cari
                       </button>
@@ -78,13 +110,23 @@ const Penyelenggara = () => {
                 <div className="d-flex filter-action">
                   <Button
                     variant="transparent"
-                    className="btn-penyelenggara-rounded btn-block bg-penyelenggara-light text-dark mr-3"
+                    className={
+                      order === "asc"
+                        ? `bg-penyelenggara-primary btn-penyelenggara-rounded btn-block  mr-3`
+                        : `bg-penyelenggara-light btn-penyelenggara-rounded btn-block  mr-3`
+                    }
+                    onClick={() => handleOrder("asc")}
                   >
                     A - Z
                   </Button>
                   <Button
                     variant="transparent"
-                    className="btn-penyelenggara-rounded mt-0 btn-block bg-penyelenggara-light text-dark"
+                    className={
+                      order === "desc"
+                        ? `bg-penyelenggara-primary btn-penyelenggara-rounded btn-block`
+                        : `bg-penyelenggara-light btn-penyelenggara-rounded btn-block`
+                    }
+                    onClick={() => handleOrder("desc")}
                   >
                     Z - A
                   </Button>
@@ -96,44 +138,57 @@ const Penyelenggara = () => {
 
         <section className="penyelenggara-content">
           <Row className="d-flex justify-content-between">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((row, i) => (
-              <Col
-                md={4}
-                lg={3}
-                xl={2}
-                sm={6}
-                xs={6}
-                className="text-center mb-10"
-              >
-                <div className="penyelenggara-mitra">
-                  <div className="penyelenggara-wrapper mx-auto d-flex align-items-center justify-content-center">
-                    <Image
-                      src="/assets/media/image-404.png"
-                      width={90}
-                      height={90}
-                      objectFit="contain"
-                    />
-                  </div>
-                  <p className="my-5 ">Google</p>
-                </div>
-              </Col>
-            ))}
+            {loading && <PulseLoaderRender />}
+            {!penyelenggara ||
+              (penyelenggara && penyelenggara.data.list_mitras.length === 0 ? (
+                <Col md={12} className="text-center mb-10 mt-11">
+                  <h1>Data Kosong</h1>
+                </Col>
+              ) : (
+                penyelenggara.data.list_mitras.map((row, i) => (
+                  <Col
+                    md={4}
+                    lg={3}
+                    xl={2}
+                    sm={6}
+                    xs={6}
+                    className="text-center mb-10"
+                    key={i}
+                  >
+                    <div className="penyelenggara-mitra">
+                      <div className="penyelenggara-wrapper mx-auto d-flex align-items-center justify-content-center">
+                        <Image
+                          src="/assets/media/image-404.png"
+                          width={90}
+                          height={90}
+                          objectFit="contain"
+                        />
+                      </div>
+                      <p className="my-5 ">{row.name}</p>
+                    </div>
+                  </Col>
+                ))
+              ))}
           </Row>
           <Row className="my-5 d-flex justify-content-center">
-            <div className="table-pagination">
-              <Pagination
-                activePage={1}
-                itemsCountPerPage={3}
-                totalItemsCount={7}
-                pageRangeDisplayed={3}
-                nextPageText={">"}
-                prevPageText={"<"}
-                firstPageText={"<<"}
-                lastPageText={">>"}
-                itemClass="page-item"
-                linkClass="page-link"
-              />
-            </div>
+            {penyelenggara &&
+              penyelenggara.data.perPage < penyelenggara.data.total && (
+                <div className="table-pagination">
+                  <Pagination
+                    activePage={page}
+                    itemsCountPerPage={penyelenggara.data.perPage}
+                    totalItemsCount={penyelenggara.data.total}
+                    pageRangeDisplayed={3}
+                    onChange={handlePagination}
+                    nextPageText={">"}
+                    prevPageText={"<"}
+                    firstPageText={"<<"}
+                    lastPageText={">>"}
+                    itemClass="page-item"
+                    linkClass="page-link"
+                  />
+                </div>
+              )}
           </Row>
         </section>
       </Container>
