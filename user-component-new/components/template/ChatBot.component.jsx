2c65d64fbd49f0990ff3846c628cb605c00debd0
firawz <ahmadfiraz@majapahit.id>
2021-11-23 11:18:35 +0700

    fixing chatbot responsive

--- user-component-new/components/template/ChatBot.component.jsx
+++ user-component-new/components/template/ChatBot.component.jsx
@@ -10,25 +10,43 @@ export default function ChatBot() {
     <div>
       <div
         className={`${style.Chatbot_container} shadow py-5`}
-        id="Chatbot_container"
         onClick={() => {
           setClicked(!clicked);
         }}
       >
-        <div className={`${style.chatbot_box} ${style.one} d-flex justify-content-end`} style={clicked ? { width: "280px" } : {}}>
-          <div className={`btn btn-block text-primary rounded-full shadow-sm ${style.chatbot_btn_help}`} style={clicked ? { right: "120px", top: 0, maxHeight: "max-content" } : {}}>
+        <div
+          className={`${style.chatbot_box} ${style.one} d-flex justify-content-end`}
+          style={clicked ? { width: "280px" } : {}}
+        >
+          <div
+            className={`btn btn-block text-primary rounded-full shadow-sm ${style.chatbot_btn_help}`}
+            style={
+              clicked
+                ? { right: "120px", top: 0, maxHeight: "max-content" }
+                : {}
+            }
+          >
             <div className={`my-3 `}>Butuh Bantuan?</div>
-            <div className={`${style.helpbtn_active}`} style={clicked ? { maxHeight: "200px" } : { maxHeight: "0px" }}>
+            <div
+              className={`${style.helpbtn_active}`}
+              style={clicked ? { maxHeight: "200px" } : { maxHeight: "0px" }}
+            >
               {clicked && (
                 <div>
-                  <div className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}>
+                  <div
+                    className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}
+                  >
                     <i className="ri-question-answer-fill" /> Live Chat
                   </div>
-                  <div className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}>
+                  <div
+                    className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}
+                  >
                     <i className="ri-draft-fill" />
                     Form Pengaduan
                   </div>
-                  <div className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}>
+                  <div
+                    className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}
+                  >
                     <i className="ri-phone-fill" />
                     Hubungi Kami
                   </div>
@@ -37,7 +55,12 @@ export default function ChatBot() {
             </div>
           </div>
           <div className={`${style.chatbot_image} d-flex justify-content-end`}>
-            <Image width={110} height={140} src={"/assets/media/ChatBot.png"} objectFit="cover" />
+            <Image
+              width={110}
+              height={140}
+              src={"/assets/media/ChatBot.png"}
+              objectFit="cover"
+            />
           </div>
         </div>
       </div>
