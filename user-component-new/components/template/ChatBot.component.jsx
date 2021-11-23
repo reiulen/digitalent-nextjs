ef6cb30de2d05de8607ebc7e023e5c7931fbe516
firawz <ahmadfiraz@majapahit.id>
2021-11-23 15:35:33 +0700

    fixing navbar

--- user-component-new/components/template/ChatBot.component.jsx
+++ user-component-new/components/template/ChatBot.component.jsx
@@ -8,65 +8,63 @@ export default function ChatBot() {
   const [clicked, setClicked] = useState(false);
 
   return (
-    <div>
+    <div
+      className={`${style.Chatbot_container} shadow`}
+      onClick={() => {
+        setClicked(!clicked);
+      }}
+    >
       <div
-        className={`${style.Chatbot_container} shadow py-5`}
-        onClick={() => {
-          setClicked(!clicked);
-        }}
+        className={`${style.chatbot_box} ${style.one} d-flex justify-content-end `}
+        style={clicked ? { width: "280px" } : {}}
       >
-        <div
-          className={`${style.chatbot_box} ${style.one} d-flex justify-content-end `}
-          style={clicked ? { width: "280px" } : {}}
+        <button
+          className={`btn btn-block text-primary rounded-full shadow-sm ${
+            style.chatbot_btn_help
+          } ${clicked && style.right_animation}`}
+          // style={
+          //   clicked ? { right: "60px", top: 0, maxHeight: "max-content" } : {}
+          // }
         >
-          <button
-            className={`btn btn-block text-primary rounded-full shadow-sm ${
-              style.chatbot_btn_help
-            } ${clicked && style.right_animation}`}
-            // style={
-            //   clicked ? { right: "60px", top: 0, maxHeight: "max-content" } : {}
-            // }
+          <div className={`my-3 `}>Butuh Bantuan?</div>
+          <div
+            className={`${style.helpbtn_active}`}
+            style={clicked ? { maxHeight: "200px" } : { maxHeight: "0px" }}
           >
-            <div className={`my-3 `}>Butuh Bantuan?</div>
-            <div
-              className={`${style.helpbtn_active}`}
-              style={clicked ? { maxHeight: "200px" } : { maxHeight: "0px" }}
-            >
-              {clicked && (
-                <div>
+            {clicked && (
+              <div>
+                <div
+                  className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}
+                >
+                  <i className="ri-question-answer-fill" /> Live Chat
+                </div>
+                <Link href="/helpdesk/formulir-pengaduan">
                   <div
                     className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}
                   >
-                    <i className="ri-question-answer-fill" /> Live Chat
+                    <i className="ri-draft-fill" />
+                    Form Pengaduan
                   </div>
-                  <Link href="/helpdesk/formulir-pengaduan">
-                    <div
-                      className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}
-                    >
-                      <i className="ri-draft-fill" />
-                      Form Pengaduan
-                    </div>
-                  </Link>
+                </Link>
 
-                  <a
-                    className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}
-                    href="tel:0213452841"
-                  >
-                    <i className="ri-phone-fill" />
-                    Hubungi Kami
-                  </a>
-                </div>
-              )}
-            </div>
-          </button>
-          <div className={`${style.chatbot_image} justify-content-end`}>
-            <Image
-              width={110}
-              height={140}
-              src={"/assets/media/ChatBot.png"}
-              objectFit="cover"
-            />
+                <a
+                  className={`${style.chatbot_options} btn-primary btn btn-block rounded-full `}
+                  href="tel:0213452841"
+                >
+                  <i className="ri-phone-fill" />
+                  Hubungi Kami
+                </a>
+              </div>
+            )}
           </div>
+        </button>
+        <div className={`${style.chatbot_image} justify-content-end`}>
+          <Image
+            width={110}
+            height={140}
+            src={"/assets/media/ChatBot.png"}
+            objectFit="cover"
+          />
         </div>
       </div>
     </div>
