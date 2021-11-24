3a307aef055bad06e0b99eae006768bea414e567
dendy.j.mjph <dendyjuliano2019@gmail.com>
2021-11-24 14:49:24 +0700

    data peserta kabadan

---
+++ pages/daftar-peserta/beasiswa.js
@@ -0,0 +1,28 @@
+import { getSession } from "next-auth/client";
+import { middlewareAuthAdminSession } from "../../utils/middleware/authMiddleware";
+
+export default function DaftarKandidatBeasiswaPage() {
+  return (
+    <>
+      <div className="d-flex flex-column flex-root"></div>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  const middleware = middlewareAuthAdminSession(session);
+  if (!middleware.status) {
+    return {
+      redirect: {
+        destination: middleware.redirect,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
