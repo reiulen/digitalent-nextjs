9b51792d1c80f6d0bcdcb7e1978d159456ab21e5
ahmad firaz <ahmadfiraz@majapahit.id>
2021-12-08 18:42:57 +0700

    fixing detail riwayat pelatihan

--- user-component-new/components/global/Riwayat-pelatihan-detail/index.jsx
+++ user-component-new/components/global/Riwayat-pelatihan-detail/index.jsx
@@ -24,8 +24,8 @@ export default function RiwayatPelatihanDetail({ session }) {
   const router = useRouter();
   const [description, setDescription] = useState(data?.deskripsi || "-");
   const [finalDescription, setFinalDescription] = useState();
-  const dateFrom = moment(data?.pendaftaran_mulai).format("LL") || "-";
-  const dateTo = moment(data?.pendaftaran_selesai).format("LL") || "-";
+  const dateFrom = moment(data?.pelatihan_mulai).format("LL") || "-";
+  const dateTo = moment(data?.pelatihan_selesai).format("LL") || "-";
   useEffect(() => {
     let newText = description.split(" ");
     let test = [];
@@ -50,7 +50,6 @@ export default function RiwayatPelatihanDetail({ session }) {
   }, []);
 
   const [truncate, setTruncate] = useState(true);
-  console.log(data, "ini data");
   return (
     <PesertaWrapper>
       <Col lg={12} className="px-0">
@@ -177,7 +176,7 @@ export default function RiwayatPelatihanDetail({ session }) {
                   <div className="d-flex">
                     <img
                       src={
-                        !data?.gambar_mitra || data?.logo
+                        !data?.gambar_mitra && data?.logo
                           ? "/assets/media/default-card.png"
                           : data?.logo
                           ? data?.file_path + data?.logo
