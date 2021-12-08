fe9e63b2b87ed4fe02bb22674eac0886f94e96e7
radengugi <radengugi@gmail.com>
2021-12-08 14:01:20 +0700

    Fix bug site management File Size & API Create

--- components/content/site-management/user/mitra/list-mitra.jsx
+++ components/content/site-management/user/mitra/list-mitra.jsx
@@ -133,12 +133,12 @@ const Table = ({ token }) => {
                       </tr>
                     </thead>
                     <tbody>
-                      {allMitraSite.data.list_mitras.length === 0 ? (
+                      {allMitraSite.data?.list_mitras.length === 0 ? (
                         <td className="align-middle text-center" colSpan="6">
                           Data Kosong
                         </td>
                       ) : (
-                        allMitraSite.data.list_mitras.map((items, index) => {
+                        allMitraSite.data?.list_mitras.map((items, index) => {
                           return (
                             <tr key={index}>
                               <td className="align-middle text-left">
@@ -198,57 +198,65 @@ const Table = ({ token }) => {
               </div>
 
               <div className="row">
-                <div className={`${stylesPag.pagination} table-pagination`}>
-                  <Pagination
-                    activePage={allMitraSite.page}
-                    itemsCountPerPage={allMitraSite.data.perPage}
-                    totalItemsCount={allMitraSite.data.total}
-                    pageRangeDisplayed={windowDimensions.width > 350 ? 3 : 1}
-                    onChange={(page) => dispatch(setPage(page))}
-                    nextPageText={">"}
-                    prevPageText={"<"}
-                    firstPageText={"<<"}
-                    lastPageText={">>"}
-                    itemClass="page-item"
-                    linkClass="page-link"
-                  />
-                </div>
-
-                <div className={`${stylesPag.rightPag} table-total ml-auto`}>
-                  <div className="row">
-                    <div className="col-4 mr-0 mt-3">
-                      <select
-                        className="form-control pr-2 cursor-pointer"
-                        id="exampleFormControlSelect2"
-                        defaultValue=""
-                        style={{
-                          width: "70px",
-                          background: "#F3F6F9",
-                          borderColor: "#F3F6F9",
-                          color: "#9E9E9E",
-                        }}
-                        onChange={(e) =>
-                          dispatch(limitCooporation(e.target.value, token))
-                        }
-                      >
-                        <option value="5">5</option>
-                        <option value="10">10</option>
-                        <option value="30">30</option>
-                        <option value="40">40</option>
-                        <option value="50">50</option>
-                      </select>
+                {allMitraSite && allMitraSite.perPage < allMitraSite.total && (
+                  <>
+                    <div className={`${stylesPag.pagination} table-pagination`}>
+                      <Pagination
+                        activePage={allMitraSite.page}
+                        itemsCountPerPage={allMitraSite.data?.perPage}
+                        totalItemsCount={allMitraSite.data?.total}
+                        pageRangeDisplayed={windowDimensions.width > 350 ? 3 : 1}
+                        onChange={(page) => dispatch(setPage(page))}
+                        nextPageText={">"}
+                        prevPageText={"<"}
+                        firstPageText={"<<"}
+                        lastPageText={">>"}
+                        itemClass="page-item"
+                        linkClass="page-link"
+                      />
                     </div>
-                    <div className="col-8 my-auto">
-                      <p
-                        className="align-middle mt-3 pt-1"
-                        style={{ color: "#B5B5C3", whiteSpace: "nowrap" }}
-                      >
-                        Total Data{" "}
-                        {allMitraSite.data && allMitraSite.data.total} List Data
-                      </p>
+                  </>
+                )}
+
+                {allMitraSite ? (
+                  <div className={`${stylesPag.rightPag} table-total ml-auto`}>
+                    <div className="row">
+                      <div className="col-4 mr-0 mt-3">
+                        <select
+                          className="form-control pr-2 cursor-pointer"
+                          id="exampleFormControlSelect2"
+                          defaultValue=""
+                          style={{
+                            width: "70px",
+                            background: "#F3F6F9",
+                            borderColor: "#F3F6F9",
+                            color: "#9E9E9E",
+                          }}
+                          onChange={(e) =>
+                            dispatch(limitCooporation(e.target.value, token))
+                          }
+                        >
+                          <option value="5">5</option>
+                          <option value="10">10</option>
+                          <option value="30">30</option>
+                          <option value="40">40</option>
+                          <option value="50">50</option>
+                        </select>
+                      </div>
+                      <div className="col-8 my-auto">
+                        <p
+                          className="align-middle mt-5 pt-1"
+                          style={{ color: "#B5B5C3"}}
+                        >
+                          Total Data{" "}
+                          {allMitraSite.data && allMitraSite.data.total} List Data
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
           </div>
