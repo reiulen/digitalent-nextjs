bdcc0fd2b8ad96c42ca087b75e280029cc7001b8
Ahmad Firaz <ahmadfiraz@gmail.com>
2021-12-09 00:15:39 +0700

    fixing navbar

--- user-component-new/components/template/Navbar.component.jsx
+++ user-component-new/components/template/Navbar.component.jsx
@@ -201,6 +201,17 @@ const Navigationbar = ({ session }) => {
     { icon: "Warning", text: "test" },
     { icon: "File", text: "test" },
   ];
+
+  const [navbarItems, setNavbarItems] = useState("");
+  const [rilisMedia, setRilisMedia] = useState([
+    "berita",
+    "artikel",
+    "galeri",
+    "video",
+  ]);
+  const [lainnya, setLainnya] = useState(["about us", "about DTS"]);
+  const [index, setIndex] = useState(0);
+
   return (
     <>
       <Navbar
@@ -298,111 +309,225 @@ const Navigationbar = ({ session }) => {
         <Navbar.Collapse id="basic-navbar-nav" className={`${style.collapsed}`}>
           <Nav className="me-auto dropdown-explore btn-masuk-peserta mr-1 d-lg-block d-none">
             {/* MENU */}
+
             <div className="row mx-1 my-0">
               <NavDropdown
                 title="Menu"
                 id="basic-nav-dropdown"
-                className="navdropdown-child position-relative w-100 text-menu"
+                className="navdropdown-child text-capitalize position-relative w-100 text-menu"
               >
-                <NavDropdown.Item href="/" className="navdropdown-child">
-                  Beranda
-                </NavDropdown.Item>
-                <div className="btn-group dropright w-100">
-                  <a
-                    type="button"
-                    className="btn rounded-0 btn-white-navbar btn-block dropdown-toggle d-flex justify-content-between align-items-center w-100"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Pelatihan
-                  </a>
-                  <div className="dropdown-menu ml-3">
-                    {akademi.map((item, i) => {
-                      return (
-                        <Link key={item.id} href={`/detail/akademi/${item.id}`}>
-                          <a className="dropdown-item navdropdown-child ">
-                            {item.slug}
-                          </a>
-                        </Link>
-                      );
-                    })}
+                <Row
+                  className={
+                    navbarItems ? `w-350px p-0 m-0` : "w-175px p-0 m-0"
+                  }
+                >
+                  <Col md={navbarItems ? 6 : 12} className="p-0 m-0">
+                    <NavDropdown.Item href="/" className="navdropdown-child">
+                      Beranda
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      className="navdropdown-child"
+                      onClick={e => {
+                        setNavbarItems(akademi);
+
+                        if (index != 1) {
+                          setIndex(1);
+                        } else {
+                          setNavbarItems(null);
+                          setIndex(0);
+                        }
+                        e.stopPropagation();
+                      }}
+                      active={index == 1 ? true : false}
+                    >
+                      <div className="d-flex justify-content-between align-items-center">
+                        Pelatihan
+                        <span className="ri-arrow-right-s-line" />
+                      </div>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="navdropdown-child">
+                      pusat informasi
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="navdropdown-child">
+                      tentang kami
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="navdropdown-child">
+                      penyelenggara
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      className="navdropdown-child"
+                      onClick={e => {
+                        setNavbarItems(rilisMedia);
+                        if (index != 2) {
+                          setIndex(2);
+                        } else {
+                          setNavbarItems(null);
+                          setIndex(0);
+                        }
+                        e.stopPropagation();
+                      }}
+                      active={index == 2 ? true : false}
+                    >
+                      <div className="d-flex justify-content-between align-items-center">
+                        rilis media
+                        <span className="ri-arrow-right-s-line" />
+                      </div>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="navdropdown-child">
+                      FAQ
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="navdropdown-child">
+                      Kontak
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={e => {
+                        setNavbarItems(lainnya);
+                        if (index != 3) {
+                          setIndex(3);
+                        } else {
+                          setNavbarItems(null);
+                          setIndex(0);
+                        }
+                        e.stopPropagation();
+                      }}
+                      active={index == 3 ? true : false}
+                      className="navdropdown-child"
+                    >
+                      <div className="d-flex justify-content-between align-items-center">
+                        lainnya
+                        <span className="ri-arrow-right-s-line" />
+                      </div>
+                    </NavDropdown.Item>
+                  </Col>
+                  <Col className="p-0 m-0 h-350px overflow-auto">
+                    {navbarItems &&
+                      navbarItems.map((el, i) => {
+                        return (
+                          <Link
+                            key={i}
+                            href={
+                              el.slug ? `/detail/akademi/${el.id}` : `/${el}`
+                            }
+                            passHref
+                          >
+                            <NavDropdown.Item className="navdropdown-child">
+                              {el.slug ? el.slug : el}
+                            </NavDropdown.Item>
+                          </Link>
+                        );
+                      })}
+                  </Col>
+                </Row>
+                {/* <Fragment>
+                  <NavDropdown.Item href="/" className="navdropdown-child">
+                    Beranda
+                  </NavDropdown.Item>
+                  <div className="btn-group dropright w-100">
+                    <a
+                      type="button"
+                      className="btn rounded-0 btn-white-navbar btn-block dropdown-toggle d-flex justify-content-between align-items-center w-100"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      Pelatihan
+                    </a>
+                    <div className="dropdown-menu ml-3">
+                      {akademi.map((item, i) => {
+                        return (
+                          <Link
+                            key={item.id}
+                            href={`/detail/akademi/${item.id}`}
+                          >
+                            <a className="dropdown-item navdropdown-child ">
+                              {item.slug}
+                            </a>
+                          </Link>
+                        );
+                      })}
+                    </div>
                   </div>
-                </div>
-                <Link href="/pusat-informasi" passHref>
-                  <NavDropdown.Item className="navdropdown-child">
-                    Pusat Informasi
-                  </NavDropdown.Item>
-                </Link>
-                <Link href="/tentang-kami" passHref>
-                  <NavDropdown.Item className="navdropdown-child">
-                    Tentang Kami
-                  </NavDropdown.Item>
-                </Link>
-                <Link href="/penyelenggara" passHref>
-                  <NavDropdown.Item className="navdropdown-child">
-                    Penyelenggara
-                  </NavDropdown.Item>
-                </Link>
-                <div className="btn-group dropright w-100">
-                  <a
-                    type="button"
-                    className="btn rounded-0 btn-white-navbar btn-block dropdown-toggle d-flex justify-content-between align-items-center w-100"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Rilis Media
-                  </a>
-                  <div className="dropdown-menu ml-3">
-                    <Link href="/berita">
-                      <a className="dropdown-item navdropdown-child">Berita</a>
-                    </Link>
-                    <Link href="/artikel">
-                      <a className="dropdown-item navdropdown-child">Artikel</a>
-                    </Link>
-                    <Link href="/galeri">
-                      <a className="dropdown-item navdropdown-child">Galeri</a>
-                    </Link>
-                    <Link href="/video">
-                      <a className="dropdown-item navdropdown-child">Video</a>
-                    </Link>
+                  <Link href="/pusat-informasi" passHref>
+                    <NavDropdown.Item className="navdropdown-child">
+                      Pusat Informasi
+                    </NavDropdown.Item>
+                  </Link>
+                  <Link href="/tentang-kami" passHref>
+                    <NavDropdown.Item className="navdropdown-child">
+                      Tentang Kami
+                    </NavDropdown.Item>
+                  </Link>
+                  <Link href="/penyelenggara" passHref>
+                    <NavDropdown.Item className="navdropdown-child">
+                      Penyelenggara
+                    </NavDropdown.Item>
+                  </Link>
+                  <div className="btn-group dropright w-100">
+                    <a
+                      type="button"
+                      className="btn rounded-0 btn-white-navbar btn-block dropdown-toggle d-flex justify-content-between align-items-center w-100"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      Rilis Media
+                    </a>
+                    <div className="dropdown-menu ml-3">
+                      <Link href="/berita">
+                        <a className="dropdown-item navdropdown-child">
+                          Berita
+                        </a>
+                      </Link>
+                      <Link href="/artikel">
+                        <a className="dropdown-item navdropdown-child">
+                          Artikel
+                        </a>
+                      </Link>
+                      <Link href="/galeri">
+                        <a className="dropdown-item navdropdown-child">
+                          Galeri
+                        </a>
+                      </Link>
+                      <Link href="/video">
+                        <a className="dropdown-item navdropdown-child">Video</a>
+                      </Link>
+                    </div>
                   </div>
-                </div>
-                <Link href="/faq" passHref>
-                  <NavDropdown.Item className="navdropdown-child">
-                    FAQ
-                  </NavDropdown.Item>
-                </Link>
-                <Link href="/kontak" passHref>
-                  <NavDropdown.Item className="navdropdown-child">
-                    Kontak
-                  </NavDropdown.Item>
-                </Link>
-                <div className="btn-group dropright w-100">
-                  <a
-                    type="button"
-                    className="btn rounded-0 btn-white-navbar btn-block dropdown-toggle d-flex justify-content-between align-items-center w-100"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Lainnya
-                  </a>
-                  <div className="dropdown-menu ml-3">
-                    {menu
-                      ? menu.map((item, index) => {
-                          return (
-                            <Link href={"/lainnya/" + item.url} key={index}>
-                              <a className="dropdown-item navdropdown-child">
-                                {item.name}
-                              </a>
-                            </Link>
-                          );
-                        })
-                      : null}
+                  <Link href="/faq" passHref>
+                    <NavDropdown.Item className="navdropdown-child">
+                      FAQ
+                    </NavDropdown.Item>
+                  </Link>
+                  <Link href="/kontak" passHref>
+                    <NavDropdown.Item className="navdropdown-child">
+                      Kontak
+                    </NavDropdown.Item>
+                  </Link>
+                  <div className="btn-group dropright w-100">
+                    <a
+                      type="button"
+                      className="btn rounded-0 btn-white-navbar btn-block dropdown-toggle d-flex justify-content-between align-items-center w-100"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      Lainnya
+                    </a>
+                    <div className="dropdown-menu ml-3">
+                      {menu
+                        ? menu.map((item, index) => {
+                            return (
+                              <Link href={"/lainnya/" + item.url} key={index}>
+                                <a className="dropdown-item navdropdown-child">
+                                  {item.name}
+                                </a>
+                              </Link>
+                            );
+                          })
+                        : null}
+                    </div>
                   </div>
-                </div>
+                </Fragment> */}
               </NavDropdown>
             </div>
             {/* END MENU */}
