377356001f898781c34042e1cad700a36b772384
ahmad firaz <ahmadfiraz@majapahit.id>
2021-12-09 23:32:29 +0700

    fixing downloadable link in rekap pendaftaran

--- components/content/pelatihan/summary/participant/file-register.jsx
+++ components/content/pelatihan/summary/participant/file-register.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const FileRegister = ({ file }) => {
+  // console.log(file, "ini file");a
+  const handleDownload = (row) => {
+    if (row.type.includes("file_doc") || row.type.includes("file_image")) {
+      window.location.href =
+        process.env.END_POINT_API_IMAGE_BEASISWA + row.value;
+    }
+  };
   return (
     <>
       <div className="card card-custom card-stretch gutter-b">
@@ -10,20 +17,26 @@ const FileRegister = ({ file }) => {
               file.map((row, i) => (
                 <div className="col-md-6" key={i}>
                   <p className="text-neutral-body my-0">{row.name}</p>
-                  <a
+
+                  <p
+                    className={
+                      row.type.includes("file_doc") ||
+                      row.type.includes("file_image")
+                        ? `text-primary`
+                        : "text-dark"
+                    }
+                    style={
+                      row.type.includes("file_doc") ||
+                      row.type.includes("file_image")
+                        ? { textDecoration: "underline", cursor: "pointer" }
+                        : {}
+                    }
                     onClick={() => {
-                      handleDownload(row.value);
+                      handleDownload(row);
                     }}
-                    href={`${process.env.END_POINT_API_IMAGE_BEASISWA}${row.value}`}
-                    style={{ cursor: "pointer" }}
                   >
-                    <p
-                      className="text-primary"
-                      style={{ textDecoration: "underline" }}
-                    >
-                      {row.value}
-                    </p>
-                  </a>
+                    {row.value}
+                  </p>
                 </div>
               ))}
           </div>
