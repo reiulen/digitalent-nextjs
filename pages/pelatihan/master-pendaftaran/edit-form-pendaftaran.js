6fdc3e77dbd812118080e5aef95848ba7f43f6c0
Ahmad Firaz <ahmadfiraz@gmail.com>
2021-12-13 14:11:37 +0700

    adding token permission in master pendaftaran

--- pages/pelatihan/master-pendaftaran/edit-form-pendaftaran.js
+++ pages/pelatihan/master-pendaftaran/edit-form-pendaftaran.js
@@ -4,16 +4,16 @@ import dynamic from "next/dynamic";
 import LoadingSkeleton from "../../../components/LoadingSkeleton";
 
 const EditForm = dynamic(
-  () =>
-    import(
-      "../../../components/content/pelatihan/master-pendaftaran/edit-master-pelatihan"
-    ),
-  {
-    loading: function loadingNow() {
-      return <LoadingSkeleton />;
-    },
-    ssr: false,
-  }
+	() =>
+		import(
+			"../../../components/content/pelatihan/master-pendaftaran/edit-master-pelatihan"
+		),
+	{
+		loading: function loadingNow() {
+			return <LoadingSkeleton />;
+		},
+		ssr: false,
+	}
 );
 
 import { getSession } from "next-auth/client";
@@ -23,39 +23,44 @@ import { getDetailMasterPelatihan } from "../../../redux/actions/pelatihan/maste
 import { getAllDataReference } from "../../../redux/actions/site-management/data-reference.actions";
 
 export default function AddTrainingPage(props) {
-  const session = props.session.user.user.data;
-  return (
-    <>
-      <div className="d-flex flex-column flex-root">
-        <EditForm token={session.token} />
-      </div>
-    </>
-  );
+	const session = props.session.user.user.data;
+	return (
+		<>
+			<div className="d-flex flex-column flex-root">
+				<EditForm token={session.token} />
+			</div>
+		</>
+	);
 }
 
 export const getServerSideProps = wrapper.getServerSideProps(
-  (store) =>
-    async ({ query, req }) => {
-      const session = await getSession({ req });
-      const middleware = middlewareAuthAdminSession(session);
-      if (!middleware.status) {
-        return {
-          redirect: {
-            destination: middleware.redirect,
-            permanent: false,
-          },
-        };
-      }
+	(store) =>
+		async ({ query, req }) => {
+			const session = await getSession({ req });
+			const middleware = middlewareAuthAdminSession(session);
+			if (!middleware.status) {
+				return {
+					redirect: {
+						destination: middleware.redirect,
+						permanent: false,
+					},
+				};
+			}
+			const token_permission = req.cookies.token_permission;
 
-      await store.dispatch(
-        getDetailMasterPelatihan(query.id, session.user.user.data.token)
-      );
-      await store.dispatch(
-        getAllDataReference(session.user.user.data.token, true)
-      );
+			await store.dispatch(
+				getDetailMasterPelatihan(
+					query.id,
+					session.user.user.data.token,
+					token_permission
+				)
+			);
+			await store.dispatch(
+				getAllDataReference(session.user.user.data.token, true)
+			);
 
-      return {
-        props: { session, title: "Tambah Pelatihan - Pelatihan" },
-      };
-    }
+			return {
+				props: { session, title: "Tambah Pelatihan - Pelatihan" },
+			};
+		}
 );
--- pages/pelatihan/master-pendaftaran/index.js
+++ pages/pelatihan/master-pendaftaran/index.js
@@ -10,50 +10,54 @@ import { getAllListMasterPelatihan } from "../../../redux/actions/pelatihan/mast
 import { getAllPermission } from "../../../redux/actions/utils/utils.actions";
 
 const MasterTraining = dynamic(
-  () =>
-    import(
-      "../../../components/content/pelatihan/master-pendaftaran/list-pelatihan.jsx"
-    ),
-  {
-    loading: function loadingNow() {
-      return <LoadingSkeleton />;
-    },
-    ssr: false,
-  }
+	() =>
+		import(
+			"../../../components/content/pelatihan/master-pendaftaran/list-pelatihan.jsx"
+		),
+	{
+		loading: function loadingNow() {
+			return <LoadingSkeleton />;
+		},
+		ssr: false,
+	}
 );
 
 export default function ListTrainingPage(props) {
-  const session = props.session.user.user.data;
-  return (
-    <>
-      <div className="d-flex flex-column flex-root">
-        <MasterTraining token={session.token} />
-      </div>
-    </>
-  );
+	const session = props.session.user.user.data;
+	return (
+		<>
+			<div className="d-flex flex-column flex-root">
+				<MasterTraining token={session.token} />
+			</div>
+		</>
+	);
 }
 
 export const getServerSideProps = wrapper.getServerSideProps(
-  (store) =>
-    async ({ query, req }) => {
-      const session = await getSession({ req });
-      const middleware = middlewareAuthAdminSession(session);
-      if (!middleware.status) {
-        return {
-          redirect: {
-            destination: middleware.redirect,
-            permanent: false,
-          },
-        };
-      }
+	(store) =>
+		async ({ query, req }) => {
+			const session = await getSession({ req });
+			const middleware = middlewareAuthAdminSession(session);
+			if (!middleware.status) {
+				return {
+					redirect: {
+						destination: middleware.redirect,
+						permanent: false,
+					},
+				};
+			}
+			const token_permission = req.cookies.token_permission;
 
-      await store.dispatch(
-        getAllListMasterPelatihan(session.user.user.data.token)
-      );
-      await store.dispatch(getAllPermission(session.user.user.data.token));
+			await store.dispatch(
+				getAllListMasterPelatihan(
+					session.user.user.data.token,
+					token_permission
+				)
+			);
+			await store.dispatch(getAllPermission(session.user.user.data.token));
 
-      return {
-        props: { session, title: "List Pelatihan - Pelatihan" },
-      };
-    }
+			return {
+				props: { session, title: "List Pelatihan - Pelatihan" },
+			};
+		}
 );
