f568e139b34a1165e0459b36837b6dcd8e36387b
ahmad firaz <ahmadfiraz@majapahit.id>
2021-12-15 14:22:29 +0700

    changing color in all skeleton loader

--- user-component-new/components/loader/LoadingFooter.jsx
+++ user-component-new/components/loader/LoadingFooter.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const LoadingFooter = () => {
-  return (
-    <>
-      <SkeletonTheme color="#ecebeb" highlightColor="ffffff">
-        <Skeleton height="570px" width="100vw" />
-      </SkeletonTheme>
-    </>
-  );
+	return (
+		<>
+			<SkeletonTheme color="#E1DFDF" highlightColor="ffffff">
+				<Skeleton height="570px" width="100vw" />
+			</SkeletonTheme>
+		</>
+	);
 };
 
 export default LoadingFooter;
