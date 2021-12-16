9da8987fb5fe70ac64b1d67bb952d39b2f282bf0
ahmad firaz <ahmadfiraz@majapahit.id>
2021-12-16 17:09:10 +0700

    fixing lainnya

--- user-component-new/content/home/lainnya/lainnya.jsx
+++ user-component-new/content/home/lainnya/lainnya.jsx
@@ -17,152 +17,152 @@ import HomeWrapper from "../../../components/wrapper/Home.wrapper";
 import styles from "../artikel/artikel.module.css";
 
 const Berita = () => {
-  const dispatch = useDispatch();
-  const router = useRouter();
+	const dispatch = useDispatch();
+	const router = useRouter();
 
-  const {
-    loading: allLoading,
-    error,
-    pages,
-    success,
-  } = useSelector((state) => state.detailPage);
+	const {
+		loading: allLoading,
+		error,
+		pages,
+		success,
+	} = useSelector((state) => state.detailPage);
 
-  // Style Select Filter
-  const customStyle = {
-    control: (styles) => ({
-      ...styles,
-      borderRadius: "30px",
-      paddingLeft: "25px",
-      fontFamily: "Poppins",
-      fontSize: "14px",
-    }),
-    multiValue: (styles) => ({
-      ...styles,
-      backgroundColor: "#E6F2FF",
-      borderRadius: "30px",
-      fontFamily: "Poppins",
-      fontSize: "14px",
-    }),
-    multiValueLabel: (styles) => ({
-      ...styles,
-      color: "#ADB5BD",
-      fontFamily: "Poppins",
-      fontSize: "14px",
-    }),
-  };
+	// Style Select Filter
+	const customStyle = {
+		control: (styles) => ({
+			...styles,
+			borderRadius: "30px",
+			paddingLeft: "25px",
+			fontFamily: "Poppins",
+			fontSize: "14px",
+		}),
+		multiValue: (styles) => ({
+			...styles,
+			backgroundColor: "#E6F2FF",
+			borderRadius: "30px",
+			fontFamily: "Poppins",
+			fontSize: "14px",
+		}),
+		multiValueLabel: (styles) => ({
+			...styles,
+			color: "#ADB5BD",
+			fontFamily: "Poppins",
+			fontSize: "14px",
+		}),
+	};
 
-  return (
-    <HomeWrapper>
-      {/* BreadCrumb */}
-      <SubHeaderComponent data={[{ link: router.asPath, name: "Lainnya" }]} />
-      <div className="row">
-        {pages.template_type === 0 && (
-          <div className="col-12 order-1">
-            <h3
-              className="card-title font-weight-bolder text-dark w-100 pb-5 my-0 my-sm-5"
-              style={{ fontSize: "40px" }}
-            >
-              {pages.property_template.title}
-            </h3>
-            <div className="" style={{ marginBottom: "48px" }}>
-              <Image
-                src={
-                  process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT +
-                  "site-management/images/" +
-                  pages.property_template.image
-                }
-                width="1700vh"
-                height="413vh"
-                objectFit="fill"
-                alt="Detail Image"
-                className="rounded-lg"
-              />
-            </div>
-            <div className="border-rounded">
-              <div className="border rounded-lg mb-5">
-                <div
-                  className="row my-5 mx-5 text-justify"
-                  style={{ overflowX: "hidden" }}
-                >
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: pages.property_template.content,
-                    }}
-                  ></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+	return (
+		<HomeWrapper>
+			{/* BreadCrumb */}
+			<SubHeaderComponent data={[{ link: router.asPath, name: pages.name }]} />
+			<div className="row">
+				{pages.template_type === 0 && (
+					<div className="col-12 order-1">
+						<h3
+							className="card-title font-weight-bolder text-dark w-100 pb-5 my-0 my-sm-5"
+							style={{ fontSize: "40px" }}
+						>
+							{pages.property_template.title}
+						</h3>
+						<div className="" style={{ marginBottom: "48px" }}>
+							<Image
+								src={
+									process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT +
+									"site-management/images/" +
+									pages.property_template.image
+								}
+								width="1700vh"
+								height="413vh"
+								objectFit="cover"
+								alt="Detail Image"
+								className="rounded-lg"
+							/>
+						</div>
+						<div className="border-rounded">
+							<div className="border rounded-lg mb-5">
+								<div
+									className="row my-5 mx-5 text-justify"
+									style={{ overflowX: "hidden" }}
+								>
+									<div
+										dangerouslySetInnerHTML={{
+											__html: pages.property_template.content,
+										}}
+									></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				)}
 
-        {pages.template_type === 1 && (
-          <div className="col-12 order-1">
-            <h3
-              className="card-title font-weight-bolder text-dark w-100 pb-5 my-0 my-sm-5"
-              style={{ fontSize: "40px" }}
-            >
-              {pages.property_template.title}
-            </h3>
-            <div className="border-rounded">
-              <div className="border rounded-lg mb-5">
-                <div
-                  className="row my-5 mx-5 text-justify"
-                  style={{ overflowX: "hidden" }}
-                >
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: pages.property_template.content,
-                    }}
-                  ></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+				{pages.template_type === 1 && (
+					<div className="col-12 order-1">
+						<h3
+							className="card-title font-weight-bolder text-dark w-100 pb-5 my-0 my-sm-5"
+							style={{ fontSize: "40px" }}
+						>
+							{pages.property_template.title}
+						</h3>
+						<div className="border-rounded">
+							<div className="border rounded-lg mb-5">
+								<div
+									className="row my-5 mx-5 text-justify"
+									style={{ overflowX: "hidden" }}
+								>
+									<div
+										dangerouslySetInnerHTML={{
+											__html: pages.property_template.content,
+										}}
+									></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				)}
 
-        {pages.template_type === 2 && (
-          <div className="col-12 order-1">
-            <h3
-              className="card-title font-weight-bolder text-dark w-100 pb-5 my-0 my-sm-5"
-              style={{ fontSize: "40px" }}
-            >
-              {pages.property_template.title}
-            </h3>
-            <div className="row">
-              <div className="col-md-4 mt-3">
-                <Image
-                  src={
-                    process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT +
-                    "site-management/images/" +
-                    pages.property_template.image
-                  }
-                  width="1500vw"
-                  height="1000vh"
-                  objectFit="fill"
-                  alt="Detail Image"
-                  className="rounded-lg"
-                />
-              </div>
-              <div className="border-rounded col-md-8 mt-3">
-                <div className="border rounded-lg mb-5">
-                  <div
-                    className="row my-5 mx-5 text-justify"
-                    style={{ overflowX: "hidden" }}
-                  >
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: pages.property_template.content,
-                      }}
-                    ></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </HomeWrapper>
-  );
+				{pages.template_type === 2 && (
+					<div className="col-12 order-1">
+						<h3
+							className="card-title font-weight-bolder text-dark w-100 pb-5 my-0 my-sm-5"
+							style={{ fontSize: "40px" }}
+						>
+							{pages.property_template.title}
+						</h3>
+						<div className="row">
+							<div className="col-md-4 mt-3">
+								<Image
+									src={
+										process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT +
+										"site-management/images/" +
+										pages.property_template.image
+									}
+									width="1500vw"
+									height="1000vh"
+									objectFit="fill"
+									alt="Detail Image"
+									className="rounded-lg"
+								/>
+							</div>
+							<div className="border-rounded col-md-8 mt-3">
+								<div className="border rounded-lg mb-5">
+									<div
+										className="row my-5 mx-5 text-justify"
+										style={{ overflowX: "hidden" }}
+									>
+										<div
+											dangerouslySetInnerHTML={{
+												__html: pages.property_template.content,
+											}}
+										></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+		</HomeWrapper>
+	);
 };
 
 export default Berita;
