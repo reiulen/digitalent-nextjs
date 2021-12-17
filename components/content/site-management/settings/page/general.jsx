b72ea3f6001d2ff9a7854a56cf1ed36730e6dfe0
Alfin <alfinramadhan@majapahit.id>
2021-12-17 12:41:25 +0700

    fix

--- components/content/site-management/settings/page/general.jsx
+++ components/content/site-management/settings/page/general.jsx
@@ -447,22 +447,22 @@ const GeneralPage = ({ token }) => {
                           <div className="image-input-wrapper">
                             {imageLogoApi === ""
                               ? imageLogo && (
-                                <Image
-                                  src={imageLogo}
-                                  layout="fill"
-                                  objectFit="fill"
-                                  alt="imageLogo"
-                                />
-                              )
+                                  <Image
+                                    src={imageLogo}
+                                    layout="fill"
+                                    objectFit="fill"
+                                    alt="imageLogo"
+                                  />
+                                )
                               : imageLogoApi && (
-                                <Image
-                                  // src={imageLogo}
-                                  src={`${process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT}site-management/images/${imageLogoApi}`}
-                                  layout="fill"
-                                  objectFit="fill"
-                                  alt="imageLogo"
-                                />
-                              )}
+                                  <Image
+                                    // src={imageLogo}
+                                    src={`${process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT}site-management/images/${imageLogoApi}`}
+                                    layout="fill"
+                                    objectFit="fill"
+                                    alt="imageLogo"
+                                  />
+                                )}
                           </div>
                           {localStorage
                             .getItem("permissions")
@@ -510,13 +510,6 @@ const GeneralPage = ({ token }) => {
                         <span className="form-text text-muted mt-6">
                           (Maksimal ukuran file 5 MB)
                         </span>
-
-                        {simpleValidator.current.message(
-                          "logoHeader",
-                          imageLogo,
-                          "",
-                          { className: "text-danger" }
-                        )}
                       </div>
                     </div>
 
@@ -529,21 +522,21 @@ const GeneralPage = ({ token }) => {
                           <div className="image-input-wrapper">
                             {imageLogoApi2 === ""
                               ? imageLogo2 && (
-                                <Image
-                                  src={imageLogo2}
-                                  layout="fill"
-                                  objectFit="fill"
-                                  alt="imageLogo"
-                                />
-                              )
+                                  <Image
+                                    src={imageLogo2}
+                                    layout="fill"
+                                    objectFit="fill"
+                                    alt="imageLogo"
+                                  />
+                                )
                               : imageLogoApi2 && (
-                                <Image
-                                  src={`${process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT}site-management/images/${imageLogoApi2}`}
-                                  layout="fill"
-                                  objectFit="fill"
-                                  alt="imageLogo"
-                                />
-                              )}
+                                  <Image
+                                    src={`${process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT}site-management/images/${imageLogoApi2}`}
+                                    layout="fill"
+                                    objectFit="fill"
+                                    alt="imageLogo"
+                                  />
+                                )}
                           </div>
                           {localStorage
                             .getItem("permissions")
@@ -574,11 +567,6 @@ const GeneralPage = ({ token }) => {
                                 type="file"
                                 accept=".png, .jpg, .jpeg .svg"
                                 onChange={(e) => onChangeImage2(e)}
-                                onBlur={() =>
-                                  simpleValidator.current.showMessageFor(
-                                    "logoFooter"
-                                  )
-                                }
                               />
                               <input type="hidden" />
                             </label>
@@ -641,29 +629,29 @@ const GeneralPage = ({ token }) => {
                               <div className="image-input image-input-outline">
                                 <div className="image-input-wrapper">
                                   {isUpdate &&
-                                    /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(
-                                      items.image_logo
-                                    )
+                                  /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(
+                                    items.image_logo
+                                  )
                                     ? items.image_logo && (
-                                      <Image
-                                        src={`${process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT}site-management/images/${items.image_logo}`}
-                                        layout="fill"
-                                        objectFit="fill"
-                                        alt="imageLogo"
-                                      />
-                                    )
+                                        <Image
+                                          src={`${process.env.END_POINT_API_IMAGE_SITE_MANAGEMENT}site-management/images/${items.image_logo}`}
+                                          layout="fill"
+                                          objectFit="fill"
+                                          alt="imageLogo"
+                                        />
+                                      )
                                     : items.image_logo && (
-                                      <Image
-                                        src={
-                                          update === true
-                                            ? items.image_logo
-                                            : `/${items.image_logo}`
-                                        }
-                                        layout="fill"
-                                        objectFit="fill"
-                                        alt="imageLogo"
-                                      />
-                                    )}
+                                        <Image
+                                          src={
+                                            update === true
+                                              ? items.image_logo
+                                              : `/${items.image_logo}`
+                                          }
+                                          layout="fill"
+                                          objectFit="fill"
+                                          alt="imageLogo"
+                                        />
+                                      )}
                                 </div>
                                 {localStorage
                                   .getItem("permissions")
