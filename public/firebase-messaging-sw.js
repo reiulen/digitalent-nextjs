78392f7ad32bdf1dd1b3e7ca959d8e4c11f98f20
dendyjuliano <dendyjuliano2019@gmail.com>
2021-12-23 22:23:26 +0700

    notif

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,13 +1,18 @@
-// import { getMessaging } from "firebase/messaging";
-// import { onBackgroundMessage } from "firebase/messaging/sw";
-
-const { getMessaging } = require("firebase/messaging");
-const { onBackgroundMessage } = require("firebase/messaging/sw");
-
-const messaging = getMessaging();
-onBackgroundMessage(messaging, (payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-});
+importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js");
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCE2YL39cNc47v0eSzmecyaAOhAKYlGjxQ",
+    authDomain: "dts-kominfo.firebaseapp.com",
+    projectId: "dts-kominfo",
+    storageBucket: "dts-kominfo.appspot.com",
+    messagingSenderId: "38242238576",
+    appId: "1:38242238576:web:8043a0824ce6e1e42134b5",
+    measurementId: "G-Z43DV3R59G",
+  });
+  firebase.messaging();
+  //background notifications will be received here
+  firebase
+    .messaging()
+    .setBackgroundMessageHandler((payload) => console.log("payload", payload));
+}
--- webPush.js
+++ webPush.js
@@ -22,8 +22,9 @@ const firebaseCloudMessaging = {
       const messaging = getMessaging();
       getToken(messaging, { vapidKey: process.env.FB_FCM_KEY_PAIR })
         .then((currentToken) => {
+          console.log("masuk sini");
+          return currentToken;
           if (currentToken) {
-            return currentToken;
           } else {
             console.log("tidak ada registrasi token");
           }
@@ -36,3 +37,46 @@ const firebaseCloudMessaging = {
   },
 };
 export { firebaseCloudMessaging };
+// importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js");
+// import localforage from "localforage";
+
+// const firebaseCloudMessaging = {
+//   //checking whether token is available in indexed DB
+//   tokenInlocalforage: async () => {
+//     return localforage.getItem("fcm_token");
+//   },
+//   //initializing firebase app
+//   init: async function () {
+//     if (!firebase.apps.length) {
+//       firebase.initializeApp({
+//         messagingSenderId: "38242238576",
+//       });
+//       try {
+//         const messaging = firebase.messaging();
+//         const tokenInLocalForage = await this.tokenInlocalforage();
+//         //if FCM token is already there just return the token
+//         if (tokenInLocalForage !== null) {
+//           return tokenInLocalForage;
+//         }
+//         //requesting notification permission from browser
+//         const status = await Notification.requestPermission();
+//         if (status && status === "granted") {
+//           //getting token from FCM
+//           const fcm_token = await messaging.getToken();
+//           if (fcm_token) {
+//             //setting FCM token in indexed db using localforage
+//             localforage.setItem("fcm_token", token);
+//             console.log("fcm token", token);
+//             //return the FCM token after saving it
+//             return token;
+//           }
+//         }
+//       } catch (error) {
+//         console.error(error);
+//         return null;
+//       }
+//     }
+//   },
+// };
+// export { firebaseCloudMessaging };
