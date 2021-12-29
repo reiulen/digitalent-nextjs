31534d08d9d0f0742d493f15f046cb21c7ad1b40
ahmad firaz <ahmadfiraz@majapahit.id>
2021-12-29 14:16:29 +0700

    handling error in form lpj dan error di beranda

--- user-component-new/content/home/beranda/Beranda.component.jsx
+++ user-component-new/content/home/beranda/Beranda.component.jsx
@@ -609,10 +609,22 @@ const Beranda = ({ session }) => {
                                                         if (!session) {
                                                           router.push("/login");
                                                         } else {
-                                                          handleBookmark(
-                                                            pelatihan[i]
-                                                              .pelatihan[j]
-                                                          );
+                                                          if (
+                                                            !session.roles.includes(
+                                                              "user"
+                                                            )
+                                                          ) {
+                                                            SweatAlert(
+                                                              "Gagal",
+                                                              "Anda sedang login sebagai Admin",
+                                                              "error"
+                                                            );
+                                                          } else {
+                                                            handleBookmark(
+                                                              pelatihan[i]
+                                                                .pelatihan[j]
+                                                            );
+                                                          }
                                                         }
                                                       }}
                                                     ></i>
@@ -862,14 +874,26 @@ const Beranda = ({ session }) => {
                                                       if (!session) {
                                                         router.push("/login");
                                                       } else {
-                                                        const pelatihan = {
-                                                          id: cardId,
-                                                          bookmark:
-                                                            cardBookmark,
-                                                        };
-                                                        handleBookmark(
-                                                          pelatihan
-                                                        );
+                                                        if (
+                                                          !session.roles.includes(
+                                                            "user"
+                                                          )
+                                                        ) {
+                                                          SweatAlert(
+                                                            "Gagal",
+                                                            "Anda sedang login sebagai Admin",
+                                                            "error"
+                                                          );
+                                                        } else {
+                                                          const pelatihan = {
+                                                            id: cardId,
+                                                            bookmark:
+                                                              cardBookmark,
+                                                          };
+                                                          handleBookmark(
+                                                            pelatihan
+                                                          );
+                                                        }
                                                       }
                                                     }}
                                                   ></i>
@@ -984,12 +1008,28 @@ const Beranda = ({ session }) => {
                                                   cardStatus === "Ditutup" &&
                                                   true
                                                 }
-                                                onClick={() =>
-                                                  handleCheckPelatihanReg(
-                                                    cardId,
-                                                    session
-                                                  )
-                                                }
+                                                onClick={() => {
+                                                  if (!session) {
+                                                    router.push("/login");
+                                                  } else {
+                                                    if (
+                                                      !session.roles.includes(
+                                                        "user"
+                                                      )
+                                                    ) {
+                                                      SweatAlert(
+                                                        "Gagal",
+                                                        "Anda sedang login sebagai Admin",
+                                                        "error"
+                                                      );
+                                                    } else {
+                                                      handleCheckPelatihanReg(
+                                                        cardId,
+                                                        session
+                                                      );
+                                                    }
+                                                  }
+                                                }}
                                                 className="btn btn-outline-primary-new rounded-pill py-3 px-12 mr-4 w-100 font-weight-bolder"
                                               >
                                                 Daftar Pelatihan
