fa87eb5e979135616825f9fb2b5f84349dee91bc
dendyjuliano <dendyjuliano2019@gmail.com>
2021-12-30 21:31:07 +0700

    save change

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ module.exports = {
     ],
   },
   env: {
-    NEXTAUTH_URL: "http://dts-dev.majapahit.id",
+    NEXTAUTH_URL: "https://dts-dev.majapahit.id",
     LOGO_DTS:
       "http://api-dts-dev.majapahit.id/pelatihan/storage/images/logo-dts.png",
     // PATH_URL: "http://dts-dev.majapahit.id",
