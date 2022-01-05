9c1cbe2607f1e367eecdfe3f07e0475526c005f8
fandiario <fandi.ario10@gmail.com>
2022-01-05 11:11:28 +0700

    Fix partnership

--- components/content/partnership/master-kategori-kerjasama/masterKategoriKerjasama.jsx
+++ components/content/partnership/master-kategori-kerjasama/masterKategoriKerjasama.jsx
@@ -207,7 +207,7 @@ const Table = ({ token }) => {
                         .list_cooperation_categories.length === 0 ? (
                         <tr>
                           <td colSpan="4" className="text-center">
-                            <h4>Data tidak ditemukan</h4>
+                            <h4>Data Kosong</h4>
                           </td>
                         </tr>
                       ) : (
