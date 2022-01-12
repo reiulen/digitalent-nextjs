316f63de936014b2ec02c94711b29095b9532830
ahmad firaz <ahmadfiraz@majapahit.id>
2022-01-12 10:42:55 +0700

    add text maxlength to 205 characters

--- components/content/sertifikat/kelola-sertifikat/nama_pelatihan/id/edit-sertifikat.jsx
+++ components/content/sertifikat/kelola-sertifikat/nama_pelatihan/id/edit-sertifikat.jsx
@@ -2110,6 +2110,7 @@ export default function EditSertifikat({ token }) {
                               onChange={(e) => handleChange(e, index)}
                               className="form-control"
                               value={syllabus}
+                              maxLength={205}
                             />
                           </div>
                           {index === 0 ? (
